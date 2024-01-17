5296e51cd8bd9e19020812b82cddeac0df0b212a
MobilobyGit <info@mobiloby.com>
2024-01-17 23:59:07 +0300

    tryrty

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -4,9 +4,9 @@
 <!-- Bootstrap Css -->
 <link href="{{ URL::asset('build/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <!-- Icons Css -->
-<link href="../build/css/icons.min.css" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('build/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
 <!-- App Css-->
-<link href="../build/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+<link href="{{ URL::asset('build/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 <!-- custom Css-->
 <link href="{{ URL::asset('build/css/custom.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 {{-- @yield('css') --}}
