668f7dd65f34ce06212d3996fb23fac2e205f5f3
MobilobyGit <info@mobiloby.com>
2024-01-18 18:02:24 +0300

    dfgfd

--- app/Http/Controllers/Mobil/LoginController.php
+++ app/Http/Controllers/Mobil/LoginController.php
@@ -12,7 +12,8 @@ class LoginController extends Controller
     public function test(Request $request)
     {
         $credentials = $request->only('user_phone');
-        try {
+        dd(JWTAuth::attempt($credentials));
+        /*try {
             if (!$token = JWTAuth::attempt($credentials)) {
                 return response()->json(['error' => 'Unauthorized'], 401);
             }
@@ -22,7 +23,7 @@ class LoginController extends Controller
 
         $user = JWTAuth::user();
 
-        return $this->respondWithToken($token, $user);
+        return $this->respondWithToken($token, $user);*/
     }
 
 
