5482f9977ba1206c26651d41fbecdcd9c54b4cdc
MobilobyGit <info@mobiloby.com>
2024-01-18 15:53:18 +0300

    sadsad

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Mobil\LoginController;
 use App\Http\Controllers\Mobil\TestDriveController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/addtestdrive', [TestDriveController::class, 'addtestdrive'])->name('addtestdrive');
 });
+Route::post('/login', [LoginController::class, 'login'])->name('login');
