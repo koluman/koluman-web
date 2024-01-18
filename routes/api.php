f9cfb9dcfbda26c26c4cf639d1a6ede8a71239ac
MobilobyGit <info@mobiloby.com>
2024-01-18 17:24:45 +0300

    ytry

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Diğer rotalarınız...
-Route::middleware(['auth:api'])->group(function () {
+
+Route::middleware(['auth:api', 'jwt.verify'])->group(function () {
     Route::post('/test', [LoginController::class, 'test'])->name('test');
     Route::post('/addtestdrive', [TestDriveController::class, 'addtestdrive'])->name('addtestdrive');
+
 });
