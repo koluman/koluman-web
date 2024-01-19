3bc79bea99b62dc9894990fee29cfc547c952d9b
MobilobyGit <info@mobiloby.com>
2024-01-19 19:48:21 +0300

    sdsad

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -11,9 +11,7 @@ class CheckRole
 {
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        if (!Auth::check()) {
-            abort(403, 'Unauthorized');
-        }
+      
     
         $user = Auth::user();
         dd($user, $roles);
