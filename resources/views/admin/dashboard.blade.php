86368a63887aed95da70394e3d2edd30995a8981
MobilobyGit <info@mobiloby.com>
2024-01-19 21:20:45 +0300

    dsf

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,7 +16,7 @@
                 <div class="col-12">
                     <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                         <div class="flex-grow-1">
-                            <h4 class="fs-16 mb-1">Hoşgeldin {{Auth::guard('web')->user()}}!</h4>
+                            <h4 class="fs-16 mb-1">Hoşgeldin {{Auth::guard('web')->user()->backuser_name}}!</h4>
                             <p class="text-muted mb-0">Here's what's happening with your store today.</p>
                         </div>
                         <div class="mt-3 mt-lg-0">
--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -763,7 +763,7 @@
                                 src="{{ URL::asset('build/images/users/avatar-1.jpg') }}" alt="Header Avatar">
                             <span class="text-start ms-xl-2">
                                 <span
-                                        class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::guard('web')->user()->user_name }}</span>
+                                        class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{ Auth::guard('web')->user()->backuser_name }}</span>
                                <span
                                     class="d-none d-xl-block ms-1 fs-12 user-name-sub-text">Hesabım</span>
                             </span>
@@ -771,7 +771,7 @@
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <h6 class="dropdown-header">Hoşgeldin {{ Auth::guard('web')->user()->user_name }}!</h6>
+                        <h6 class="dropdown-header">Hoşgeldin {{ Auth::guard('web')->user()->backuser_name }}!</h6>
                         <a class="dropdown-item" href="pages-profile"><i
                                 class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
                                 class="align-middle">Profilim</span></a>
