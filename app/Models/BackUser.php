f3cebea335ed867208140b8228986189c1bfec00
MobilobyGit <info@mobiloby.com>
2024-01-19 16:51:06 +0300

    sdfsdf

--- app/Models/BackUser.php
+++ app/Models/BackUser.php
@@ -2,13 +2,15 @@
 
 
 namespace App\Models;
-
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class BackUser extends Authenticatable
+
+class BackUser extends Authenticatable implements JWTSubject 
+
 {
     use HasApiTokens, HasFactory, Notifiable;
 
