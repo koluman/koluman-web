0ae7030507db925f68953e91eb906309d76f4159
MobilobyGit <info@mobiloby.com>
2024-01-19 19:19:17 +0300

    dfsdf

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -29,7 +29,7 @@ class CheckRole
         // Yetki yoksa isteği reddet
         abort(403, 'Unauthorized');*/
         // Kullanıcı modelinde role alanının olup olmadığını ve dolu mu kontrol et
-        $user = auth()->user();
+        $user = Auth::user();
         dd($user); // Kullanıcıyı ekrana yazdırarak kontrol et
     }
 }
