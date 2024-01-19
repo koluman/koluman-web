990c39ccbc810816c71026ad2149509188fe59f8
MobilobyGit <info@mobiloby.com>
2024-01-19 16:15:51 +0300

    sdsd

--- app/Models/TestDrive.php
+++ app/Models/TestDrive.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class TestDrive extends Model
 {
     use HasFactory;
+    protected $table = 'test_drive'; // Modelin "users" tablosuna bağlı olduğunu belirtir
+    protected $primaryKey = 'drive_id';
 }
