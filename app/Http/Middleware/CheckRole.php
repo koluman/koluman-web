11392bf9932ebee079c9d434853fd00ab74de65a
MobilobyGit <info@mobiloby.com>
2024-01-19 17:49:34 +0300

    aSA

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -10,38 +10,28 @@ class CheckRole
 {
     public function handle(Request $request, Closure $next, ...$roles)
     {
-        /*if (Auth::guard('web')->check()) {
-            $user = Auth::guard('web')->user();
-    
-            foreach ($roles as $role) {
-                if ($user->backuser_role === $role) {
-                    return $next($request);
-                }
-            }
-    
-            // If the user does not have the required role, return a 403 error
-            abort(403, 'Unauthorized action.');
-        }
-   
-        
-        // Giriş yapmamışsa ana sayfaya yönlendir
-        return redirect('/'); */
         $this->authenticate($request);
-
+    
         // Kullanıcıyı al
         $user = auth('web')->user();
-
-        /*foreach ($roles as $role) {
-            if ($user->role === $role) {
+    
+        if (!$user) {
+            // Kullanıcı oturumu açık değilse, 401 hatası döndür
+            abort(401, 'Unauthorized');
+        }
+    
+        // Kullanıcının yetkilerini kontrol et
+        foreach ($roles as $role) {
+            // Kullanıcının rolü, verilen rollerden birine eşitse, middleware'ı geç
+            if (isset($user['role']) && $user['role'] === $role) {
                 return $next($request);
             }
         }
-
+    
         // Kullanıcının yetkisi yoksa 403 hatası döndür
-        return abort(403, 'Unauthorized action.');*/
-
-        dd($user);
+        return abort(403, 'Unauthorized action.');
     }
+    
     // CheckRole middleware içinde kullanım örneği
     public function authenticate($request)
     {
