394382a12cbdf28c342d9037d9f12dc7bfcd013a
MobilobyGit <info@mobiloby.com>
2024-01-22 12:10:58 +0300

    dfdf

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -117,6 +117,8 @@
                     </div>
                 </li> <!-- end Dashboard Menu -->
                @if(Auth::guard('web')->user()->backuser_role=='admin')
+               @dump(@lang)
+               @dump(Session::get('lang'));
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarUsers" data-bs-toggle="collapse" role="button"
                         aria-expanded="false" aria-controls="sidebarUsers">
