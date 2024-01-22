ccca97bd6fdfa2c2fe9fae2d588d8828625e06cb
MobilobyGit <info@mobiloby.com>
2024-01-22 19:17:37 +0300

    sadasd

--- app/Http/Requests/TestDriveAddRequest.php
+++ app/Http/Requests/TestDriveAddRequest.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Http\Exceptions\HttpResponseException;
 class TestDriveAddRequest extends FormRequest
 {
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => 0,
+            'message' => $validator->errors()->first(),
+            'errors' => $validator->errors(),
+        ], 422));
+    }
     public function authorize()
     {
         return true;
