dd2f8261d609ebf7c72ff3518bc5f2605736a9c9
MobilobyGit <info@mobiloby.com>
2024-01-22 11:10:22 +0300

    sdd

--- build/js/panel/login.js
+++ build/js/panel/login.js
@@ -2,7 +2,8 @@ $( "#loginbutton" ).click(function() {
        var csrfToken = $('meta[name="csrf-token"]').attr('content');
         var email = $("#username").val();
         var password = $("#password-input").val();
-        $.ajax({
+        console.log(email+"--"+password);
+       /* $.ajax({
           type: 'POST',
           url: 'https://mobiloby.app/koluman/web/login',
           data: {
@@ -13,11 +14,11 @@ $( "#loginbutton" ).click(function() {
           //dataType: 'json',
           success: function (data) {
             console.log(data);
-            /*if (data.success == 1) {
+            if (data.success == 1) {
               window.location.href = "https://mobiloby.app/koluman/web/"+data.redirectRoute; 
             } else {
                 alert(data.message); // SMS gönderimi başarısızsa kullanıcıya bir uyarı göster
-            }*/
+            }
           }
-      });
+      });*/
   });
