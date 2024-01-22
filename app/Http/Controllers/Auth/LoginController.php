6db97df7f121ab7978ab9f0c480932f4cab6db33
MobilobyGit <info@mobiloby.com>
2024-01-22 11:05:18 +0300

    sddffd

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -61,7 +61,7 @@ class LoginController extends Controller
                 'redirectRoute' => "",
             ];
         }
-        return response()->json($responseData);
+        //return response()->json($responseData);
     }
 
 
