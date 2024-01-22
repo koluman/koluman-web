5aa4014aded9ef70587607a6b596c8c58b1ded77
MobilobyGit <info@mobiloby.com>
2024-01-22 18:01:34 +0300

    asds

--- app/Http/Controllers/Mobil/LoginController.php
+++ app/Http/Controllers/Mobil/LoginController.php
@@ -26,31 +26,36 @@ class LoginController extends Controller
                     Auth::guard('api')->login($user);
                     $token = JWTAuth::fromUser($user);
                     $u = JWTAuth::setToken($token)->authenticate();
-                    $reftoken = JWTAuth::parseToken()->refresh();
-                    $responseData = [
-                        "success" => 1,
-                        "token" => $token,
-                        "refreshtoken" => $reftoken,
-                        'user' => $u,
-                        "message" => "Login İşlemi başarılı",
-                    ];
+                    $authenticatedUser = JWTAuth::setToken($token)->authenticate();
+
+                    if ($authenticatedUser) {
+                        $refreshToken = JWTAuth::refresh($token);
+
+                        $responseData = [
+                            "success" => 1,
+                            "token" => $token,
+                            "refreshtoken" => $refreshToken,
+                            'user' => $authenticatedUser,
+                            "message" => "Login İşlemi başarılı",
+                        ];
+                    } else {
+                        $responseData = [
+                            "success" => 0,
+                            "token" => "",
+                            "refreshtoken" => "",
+                            'user' => "",
+                            "message" => "Login İşlemi başarısız",
+                        ];
+                    }
                 } else {
                     $responseData = [
                         "success" => 0,
                         "token" => "",
                         "refreshtoken" => "",
                         'user' => "",
-                        "message" => "Login İşlemi başarısız",
+                        "message" => "Token bilgisi gelmedi, lütfen tokenı yollayınız",
                     ];
                 }
-            } else {
-                $responseData = [
-                    "success" => 0,
-                    "token" => "",
-                    "refreshtoken" => "",
-                    'user' => "",
-                    "message" => "Token bilgisi gelmedi, lütfen tokenı yollayınız",
-                ];
             }
         } catch (\Exception $e) {
             Log::error('Login Error: ' . $e->getMessage());
@@ -63,7 +68,6 @@ class LoginController extends Controller
             ];
         }
         return response()->json($responseData);
-
     }
     public function userlogout(Request $request)
     {
@@ -98,48 +102,48 @@ class LoginController extends Controller
             $token = $request->header('Authorization');
             $token = str_replace('Basic ', '', $token);
             if ($token) {
-            $user_identity = $request->user_identity;
-            $user_phone = $request->user_phone;
-            $user_name = $request->user_name;
+                $user_identity = $request->user_identity;
+                $user_phone = $request->user_phone;
+                $user_name = $request->user_name;
 
-            $existingUser = User::where('user_phone', $user_phone)->first();
-            if ($existingUser) {
+                $existingUser = User::where('user_phone', $user_phone)->first();
+                if ($existingUser) {
+                    $responseData = [
+                        "success" => 0,
+                        "message" => "Bu kullanıcı daha önce kayıt edilmiş, lütfen giriş yapınız",
+                        "user" => "",
+                        "token" => "",
+                        "refreshtoken" => "",
+                    ];
+                } else {
+                    $user_id = 'koluman_' . round(microtime(true) * 1000) . '_' . rand(100000, 999999);
+                    $user = User::create([
+                        'user_id' => $user_id,
+                        'user_name' => $user_name,
+                        'user_phone' => $user_phone,
+                        'user_identity' => $user_identity,
+                        'user_register_date' => Carbon::now('Europe/Istanbul'),
+                    ]);
+                    $token = JWTAuth::fromUser($user);
+                    $u = JWTAuth::setToken($token)->authenticate();
+                    $reftoken = JWTAuth::parseToken()->refresh();
+                    $responseData = [
+                        "success" => 1,
+                        "message" => "Kullanıcı Kayıt edildi",
+                        "user" => $user,
+                        "token" => $token,
+                        "refreshtoken" => $reftoken,
+                    ];
+                }
+            } else {
                 $responseData = [
                     "success" => 0,
-                    "message" => "Bu kullanıcı daha önce kayıt edilmiş, lütfen giriş yapınız",
+                    "message" => "Token bulunamadı, Logout işlemi başarısız",
                     "user" => "",
                     "token" => "",
-                    "refreshtoken" => "",
-                ];
-            } else {
-                $user_id = 'koluman_' . round(microtime(true) * 1000) . '_' . rand(100000, 999999);
-                $user = User::create([
-                    'user_id' => $user_id,
-                    'user_name' => $user_name,
-                    'user_phone' => $user_phone,
-                    'user_identity' => $user_identity,
-                    'user_register_date' => Carbon::now('Europe/Istanbul'),
-                ]);
-                $token = JWTAuth::fromUser($user);
-                $u = JWTAuth::setToken($token)->authenticate();
-                $reftoken = JWTAuth::parseToken()->refresh();
-                $responseData = [
-                    "success" => 1,
-                    "message" => "Kullanıcı Kayıt edildi",
-                    "user" => $user,
-                    "token" => $token,
-                    "refreshtoken" => $reftoken,
+                    "refreshtoken" => ""
                 ];
             }
-        } else {
-            $responseData = [
-                "success" => 0,
-                "message" => "Token bulunamadı, Logout işlemi başarısız",
-                "user" => "",
-                "token" => "",
-                "refreshtoken" => ""
-            ];
-        }
         } catch (\Exception $e) {
             $responseData = [
                 "success" => 0,
