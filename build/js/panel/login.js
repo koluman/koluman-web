fb6ab63e8fdf0c7ed726f161d9b7a54475835dc7
MobilobyGit <info@mobiloby.com>
2024-01-22 11:29:04 +0300

    sdsad

--- build/js/panel/login.js
+++ build/js/panel/login.js
@@ -30,12 +30,11 @@ $( "#loginbutton" ).click(function() {
         },
         dataType: 'json',
         success: function (data) {
-          /*if (data.success == 1) {
+          if (data.success == 1) {
             window.location.href = "https://mobiloby.app/koluman/web/signin"; 
           } else {
               alert(data.message); 
-          }*/
-          console.log(data);
+          }
         }
     });
   }
