1aa59569dabc4620294ae5f319f267c93227ca18
MobilobyGit <info@mobiloby.com>
2024-01-23 11:49:43 +0300

    dfdsf

--- app/Http/Controllers/Mobil/LoginController.php
+++ app/Http/Controllers/Mobil/LoginController.php
@@ -35,7 +35,7 @@ class LoginController extends Controller
     
                     if ($authenticatedUser) {
                         // Refresh token'ı oluştururken süreyi 14 gün olarak belirle
-                        $refreshToken = JWTAuth::getToken();
+                        $refreshToken = JWTAuth::refresh($originalToken, 602414);
 
                         $responseData = [
                             "success" => 1,
