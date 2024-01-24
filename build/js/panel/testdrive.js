6b1d46276b68420e1136c28594c643b605b30ee2
MobilobyGit <info@mobiloby.com>
2024-01-24 17:45:51 +0300

    aSAs

--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -17,12 +17,10 @@ function getshowroom() {
                     dataType: 'json',
                     success: function (data) {
                         if (data.success == 1) {
-                           console.log(data);
                            var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
                            for (i = 0; i < data.shoowroom.length; ++i) {
-                               option += "<option value='" + data.shoowroom[i]["car_id"] + "'>" + data.shoowroom[i]["car_id"] + "</option>";
+                               option += "<option value='" + data.shoowroom[i]["car_id"] + "'>" + data.shoowroom[i]["car_name"] + "</option>";
                            }
-                           console.log(option);
                            $('#event-category').html('');
                            $('#event-category').html(option);
                         }
