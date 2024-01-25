279fa8b29be69374de11091264566de2f8dcc85c
MobilobyGit <info@mobiloby.com>
2024-01-25 17:21:19 +0300

    dsds

--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -23,7 +23,6 @@ function getshowroom() {
                             }
                             $('#event-category').html(option);
                         }
-
                     }
                 });
             } else {
