0a049c305f50b40b4984492c65223042e5ae0d52
MobilobyGit <info@mobiloby.com>
2024-01-25 17:27:17 +0300

    ass

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
 Route::get('/getApiToken', [HomeController::class, 'getApiToken'])->name('getApiToken');
 Route::get('/getBasicToken', [HomeController::class, 'getBasicToken'])->name('getBasicToken');
-
 Route::middleware(['prevent-back-history', 'checkRole:admin'])->group(function () {
     Route::get('/admindashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/adminusers', [HomeController::class, 'users'])->name('admin.users');
