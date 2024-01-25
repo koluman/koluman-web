73b09b1cd140c95d7e913dca774167d9094ceab8
MobilobyGit <info@mobiloby.com>
2024-01-25 11:28:45 +0300

    dfgfdg

--- build/js/panel/login.js
+++ build/js/panel/login.js
@@ -1,22 +1,22 @@
-$("#loginbutton").click(function () {
-    var email = $("#username").val();
-    var password = $("#password-input").val();
-    $.ajax({
-        type: 'POST',
-        url: 'https://mobiloby.app/koluman/web/login',
-        data: {
+$( "#loginbutton" ).click(function() {
+       var csrfToken = $('meta[name="csrf-token"]').attr('content');
+        var email = $("#username").val();
+        var password = $("#password-input").val();
+        $.ajax({
+          type: 'POST',
+          url: 'https://mobiloby.app/koluman/web/login',
+          data: {
             email: email,
             password: password,
-        },
-        dataType: 'json',
-        success: function (data) {
-          console.log(data);
-            /*if (data.success == 1) {
-                window.location.href = "https://mobiloby.app/koluman/web/" + data.redirectRoute;
+              _token: csrfToken, // CSRF token'ını gönder
+          },
+          dataType: 'json',
+          success: function (data) {
+            if (data.success == 1) {
+              window.location.href = "https://mobiloby.app/koluman/web/"+data.redirectRoute; 
             } else {
-                alert(data.message);
-            }*/
-        }
-
-    });
-});
+                alert(data.message); 
+            }
+          }
+      });
+  });
