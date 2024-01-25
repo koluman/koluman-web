a428a1e22befd0960ca0d9ca67430ceb4b81b58f
MobilobyGit <info@mobiloby.com>
2024-01-25 16:16:14 +0300

    sdfdsf

--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -4,18 +4,36 @@ $(document).ready(function () {
 
 function getshowroom() {
     $.ajax({
+        url: 'https://mobiloby.app/koluman/web/getBasicToken',
         type: 'GET',
-        url: 'https://mobiloby.app/koluman/web/api/getshoowroom',
-        dataType: 'json',
-        success: function (data) {
-            if (data.success == 1) {
-                var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
-                for (i = 0; i < data.shoowroom.length; ++i) {
-                    option += "<option value='" + data.shoowroom[i]["car_id"] + "'>" + data.shoowroom[i]["car_name"] + "</option>";
-                }
-                $('#event-category').html(option);
-            }
+        success: function (response) {
+            if (response.success == 1) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://mobiloby.app/koluman/web/api/getshowroomcars',
+                    headers: {
+                        'Authorization': 'Basic ' + response.token
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        if (data.success == 1) {
+                            var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
+                            for (i = 0; i < data.shoowroom.length; ++i) {
+                                option += "<option value='" + data.shoowroom[i]["car_id"] + "'>" + data.shoowroom[i]["car_name"] + "</option>";
+                            }
+                            $('#event-category').html(option);
+                        }
 
+                    }
+                });
+            } else {
+                alert(response.message);
+            }
+        },
+        error: function (error) {
+            console.error(error);
         }
     });
+
 }
+
