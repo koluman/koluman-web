9bec088116799c5bc7e95355513451bed55b21da
MobilobyGit <info@mobiloby.com>
2024-01-25 11:18:34 +0300

    sdfds

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,16 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [AuthController::class, 'signin'])->name('signin');
-Route::post('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('index/{locale}', [App\Http\Controllers\HomeController::class, 'lang']);
 Route::get('/getApiToken', [HomeController::class, 'getApiToken'])->name('getApiToken');
 Route::get('/getBasicToken', [HomeController::class, 'getBasicToken'])->name('getBasicToken');
 
-Route::middleware(['jwt.verify','prevent-back-history','checkRole:admin'])->group(function () {
+Route::middleware(['prevent-back-history','checkRole:admin'])->group(function () {
+    Route::middleware(['jwt.verify'])->group(function () {
+        Route::post('/login', [LoginController::class, 'login'])->name('login');
+
+    });
     Route::get('/admindashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
     Route::get('/adminusers', [HomeController::class, 'users'])->name('admin.users');
     Route::post('/getallusers', [BackUsersController::class, 'getallusers'])->name('getallusers');
