4d53a875082c665787b6fad3f3657178cd585ad0
MobilobyGit <info@mobiloby.com>
2024-01-29 16:18:00 +0300

    sdfsdf

--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -2,49 +2,51 @@ document.addEventListener("DOMContentLoaded", function () {
     getshowroom();
     getapiusers();
     $("#appointment_date").change(function () {
-        function getdate() {
-            // Seçilen tarihi al
-            var selectedDate = $("#appointment_date").val();
-            var selectedCar = $("#car_id").val();
-            $.ajax({
-                type: 'POST',
-                url: 'https://mobiloby.app/koluman/web/testdriveschedules',
-                data: {
-                    selectedDate: selectedDate,
-                    selectedCar,
-                    selectedCar,
-                    _token: csrfToken, // CSRF token'ını gönder
-                },
-                dataType: 'json',
-                success: function (data) {
-                    if (data.success == 1) {
-                        var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
-                        // Tüm saatleri döngüye al ve dolu olanları işaretle
-                        var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
-
-                        for (var i = 0; i < allTimes.length; i++) {
-                            var time = allTimes[i];
-                            var isTimeOccupied = data.schedules.some(schedule => schedule.appointment_time === time);
-
-                            if (isTimeOccupied) {
-                                option += "<option value='" + time + "'>" + time + "</option>";
-                            } else {
-                                option += "<option value='" + time + "'>" + time + "</option>";
-                            }
-                        }
-                        console.log(option);
-                        $('#appointment_time').html('');
-                        $('#appointment_time').html(option);
-
-                    }
-
-                }
-            });
-        };
+       getdate();
     });
 });
 
 var csrfToken = $('meta[name="csrf-token"]').attr('content');
+function getdate() {
+    console.log("dsad");
+    // Seçilen tarihi al
+    var selectedDate = $("#appointment_date").val();
+    var selectedCar = $("#car_id").val();
+    $.ajax({
+        type: 'POST',
+        url: 'https://mobiloby.app/koluman/web/testdriveschedules',
+        data: {
+            selectedDate: selectedDate,
+            selectedCar,
+            selectedCar,
+            _token: csrfToken, // CSRF token'ını gönder
+        },
+        dataType: 'json',
+        success: function (data) {
+            if (data.success == 1) {
+                var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
+                // Tüm saatleri döngüye al ve dolu olanları işaretle
+                var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
+
+                for (var i = 0; i < allTimes.length; i++) {
+                    var time = allTimes[i];
+                    var isTimeOccupied = data.schedules.some(schedule => schedule.appointment_time === time);
+
+                    if (isTimeOccupied) {
+                        option += "<option value='" + time + "'>" + time + "</option>";
+                    } else {
+                        option += "<option value='" + time + "'>" + time + "</option>";
+                    }
+                }
+                console.log(option);
+                $('#appointment_time').html('');
+                $('#appointment_time').html(option);
+
+            }
+
+        }
+    });
+};
 function getshowroom() {
     $.ajax({
         type: 'GET',
