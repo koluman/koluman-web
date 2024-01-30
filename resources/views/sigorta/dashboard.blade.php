d02711dcf07930a9806ab2cbf0ea3d44cce14a61
MobilobyGit <info@mobiloby.com>
2024-01-30 10:55:32 +0300

    dsfdsf

--- resources/views/sigorta/dashboard.blade.php
+++ resources/views/sigorta/dashboard.blade.php
@@ -18,7 +18,7 @@
                     <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                         <div class="flex-grow-1">
                             <h4 class="fs-16 mb-1">Hoşgeldin !</h4>
-                            <p class="text-muted mb-0">Here's what's happening with your store today.</p>
+                            <p class="text-muted mb-0">MErhabalaerrr.</p>
                         </div>
                         <div class="mt-3 mt-lg-0">
                             <form action="javascript:void(0);">
