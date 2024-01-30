f1308501b100bf62a4905315642eb7d62e9337ce
MobilobyGit <info@mobiloby.com>
2024-01-30 11:02:16 +0300

    sdfdsf

--- resources/views/sigorta/dashboard.blade.php
+++ resources/views/sigorta/dashboard.blade.php
@@ -10,13 +10,14 @@
 
 <div class="row">
     <div class="col">
+
         <div class="h-100">
             <div class="row mb-3 pb-1">
                 <div class="col-12">
                     <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                         <div class="flex-grow-1">
-                            <h4 class="fs-16 mb-1">Hoşgeldin !</h4>
-                            <p class="text-muted mb-0">MErhabalaerrr.</p>
+                            <h4 class="fs-16 mb-1">Hoşgeldin {{Auth::guard('web')->user()->backuser_name}}!</h4>
+                            <p class="text-muted mb-0">Here's what's happening with your store today.</p>
                         </div>
                         <div class="mt-3 mt-lg-0">
                             <form action="javascript:void(0);">
