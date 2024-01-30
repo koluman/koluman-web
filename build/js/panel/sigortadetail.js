a92317f768b7e177bcc17cedd7f470c570bf2897
MobilobyGit <info@mobiloby.com>
2024-01-30 17:23:58 +0300

    sdfdf

--- build/js/panel/sigortadetail.js
+++ build/js/panel/sigortadetail.js
@@ -33,6 +33,8 @@ function getdetail(id){
                 $("#insurance_price").val(a.insurance_price);
                 $("#insurance_description").text(a.insurance_description);
                 $("#insurance_end_date").val(a.insurance_end_date);
+                $("#user_id").val(a.user_id);
+                $("#insurance_state").val(a.insurance_state);
 
             }
 
