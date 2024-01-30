6c61bcfc6667e3d43c7b5e131c08ee99b4562b83
MobilobyGit <info@mobiloby.com>
2024-01-30 15:36:33 +0300

    dfgdf

--- resources/views/sigorta/detail.blade.php
+++ resources/views/sigorta/detail.blade.php
@@ -132,14 +132,18 @@
                 <button type="submit" class="btn btn-success w-sm">Create</button>
             </div>
         </div>
-          <div class="text-end mb-12">
-                <button type="submit" class="btn btn-danger w-sm">Delete</button>
-                <button type="submit" class="btn btn-secondary w-sm">Draft</button>
-                <button type="submit" class="btn btn-success w-sm">Create</button>
-            </div>
         <!-- end col -->
         <div class="col-lg-4">
-         
+            <div class="text-end mb-4">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <button type="submit" class="btn btn-danger w-sm">Delete</button>
+                    </div>
+                    <div class="col-lg-6">
+                        <button type="submit" class="btn btn-secondary w-sm">Draft</button>
+                    </div>
+                </div>
+            </div>
             <div class="card">
                 <div class="card-header">
                     <h5 class="card-title mb-0">Durum</h5>
