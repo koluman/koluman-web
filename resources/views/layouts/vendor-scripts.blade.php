4c8b046279dade7866a13c795a216221f071d1fc
MobilobyGit <info@mobiloby.com>
2024-01-30 16:53:59 +0300

    dfgdfg

--- resources/views/layouts/vendor-scripts.blade.php
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -4,5 +4,6 @@
 <script src="{{ URL::asset('build/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ URL::asset('build/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
 <script src="{{ URL::asset('build/js/plugins.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 @yield('script')
 @yield('script-bottom')
