cc77b268f41b3ce9b3518d03ddce253080cb46b9
MobilobyGit <info@mobiloby.com>
2024-01-30 16:20:57 +0300

    fsdfsf

--- build/js/plugins.js
+++ build/js/plugins.js
@@ -11,6 +11,6 @@ File: Common Plugins Js File
 if(document.querySelectorAll("[toast-list]") || document.querySelectorAll('[data-choices]') || document.querySelectorAll("[data-provider]")){ 
 
   document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'></script>");
-  document.writeln("<script type='text/javascript' src='build/libs/choices.js/public/assets/scripts/choices.min.js'></script>");
-  document.writeln("<script type='text/javascript' src='build/libs/flatpickr/flatpickr.min.js'></script>");    
+  document.writeln("<script type='text/javascript' src='https://mobiloby.app/koluman/web/build/libs/flatpickr/flatpickr.min.js'></script>");
+  document.writeln("<script type='text/javascript' src='https://mobiloby.app/koluman/web/build/libs/flatpickr/flatpickr.min.js'></script>");    
 }
