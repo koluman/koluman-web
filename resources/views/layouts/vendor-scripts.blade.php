84219545464cb77aafc05d4534dbc24027f873b3
MobilobyGit <info@mobiloby.com>
2024-01-30 16:21:35 +0300

    dsfdsf

--- resources/views/layouts/vendor-scripts.blade.php
+++ resources/views/layouts/vendor-scripts.blade.php
@@ -3,6 +3,6 @@
 <script src="{{ URL::asset('build/libs/node-waves/waves.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/feather-icons/feather.min.js') }}"></script>
 <script src="{{ URL::asset('build/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
-<script src="https://mobiloby.app/koluman/web/build/js/plugins.js"></script>
+<script src="{{ URL::asset('build/js/plugins.js') }}"></script>
 @yield('script')
 @yield('script-bottom')
