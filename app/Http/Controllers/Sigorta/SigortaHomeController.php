b1592684b953af095ea729de2784c1415126f570
MobilobyGit <info@mobiloby.com>
2024-01-30 10:35:01 +0300

    asdds

---
+++ app/Http/Controllers/Sigorta/SigortaHomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Sigorta;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SigortaHomeController extends Controller
+{
+    public function dashboard(Request $request)
+    {
+        return view('sigorta.dashboard');
+    }
+}
