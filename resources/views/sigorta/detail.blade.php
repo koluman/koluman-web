a8dbea0adac38ef63f3da3e2710a8fd6b9c373d1
MobilobyGit <info@mobiloby.com>
2024-01-31 15:56:57 +0300

    sdfds

--- resources/views/sigorta/detail.blade.php
+++ resources/views/sigorta/detail.blade.php
@@ -16,175 +16,169 @@
     @endcomponent
     <div class="row">
         <form id="sigortaForm" method="post" enctype="multipart/form-data">
-            @csrf
+        @csrf
+        <div class="col-lg-8">
+            
+            <div class="card">
+                <div class="card-body">
 
-            <div class="col-lg-8">
-                <div class="card">
-                    <div class="card-body">
-
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="mb-3 mb-lg-0">
-                                    <label for="choices-status-input" class="form-label">Poliçe Fiyat</label>
-                                    <input type="text" class="form-control" id="insurance_price" name="insurance_price">
-                                </div>
-                            </div>
-                            <input type="hidden" class="form-control" id="insurance_id" name="insurance_id">
-                            <div class="col-lg-6 mb-3">
-                                <div>
-                                    <label for="datepicker-deadline-input" class="form-label">Poliçe Bitiş Tarih</label>
-                                    <input type="text" class="form-control" id="insurance_end_date"
-                                        name="insurance_end_date" data-provider="flatpickr">
-                                </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="mb-3 mb-lg-0">
+                                <label for="choices-status-input" class="form-label">Poliçe Fiyat</label>
+                                <input type="text" class="form-control" id="insurance_price" name="insurance_price">
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-12 mb-3">
-                                <label class="form-label">Sigorta Açıklama</label>
-                                <textarea class="form-control" name="insurance_description" id="insurance_description" rows="3"></textarea>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <div class="mb-3 mb-lg-0">
-                                    <label for="choices-priority-input" class="form-label">Talep Tarihi</label>
-                                    <input type="text" class="form-control" id="insurance_request_date"
-                                        name="insurance_request_date" data-provider="flatpickr">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div class="mb-3 mb-lg-0">
-                                    <label for="choices-status-input" class="form-label">İnceleme Tarihi</label>
-                                    <input type="text" class="form-control" id="insurance_review_date"
-                                        name="insurance_review_date" data-provider="flatpickr">
-                                </div>
-                            </div>
-                            <div class="col-lg-4">
-                                <div>
-                                    <label for="datepicker-deadline-input" class="form-label">Sonuç Tarihi</label>
-                                    <input type="text" class="form-control" id="insurance_result_date"
-                                        name="insurance_result_date" data-provider="flatpickr">
-
-                                </div>
+                         <input type="hidden" class="form-control" id="insurance_id" name="insurance_id">
+                        <div class="col-lg-6 mb-3">
+                            <div>
+                                <label for="datepicker-deadline-input" class="form-label">Poliçe Bitiş Tarih</label>
+                                <input type="text" class="form-control" id="insurance_end_date" name="insurance_end_date"
+                                    data-provider="flatpickr">
                             </div>
                         </div>
                     </div>
-                    <!-- end card body -->
+                    <div class="row">
+                        <div class="col-lg-12 mb-3">
+                            <label class="form-label">Sigorta Açıklama</label>
+                            <textarea class="form-control" name="insurance_description" id="insurance_description" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="mb-3 mb-lg-0">
+                                <label for="choices-priority-input" class="form-label">Talep Tarihi</label>
+                                <input type="text" class="form-control" id="insurance_request_date"
+                                    name="insurance_request_date" data-provider="flatpickr">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="mb-3 mb-lg-0">
+                                <label for="choices-status-input" class="form-label">İnceleme Tarihi</label>
+                                <input type="text" class="form-control" id="insurance_review_date"
+                                    name="insurance_review_date" data-provider="flatpickr">
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div>
+                                <label for="datepicker-deadline-input" class="form-label">Sonuç Tarihi</label>
+                                <input type="text" class="form-control" id="insurance_result_date"
+                                    name="insurance_result_date" data-provider="flatpickr">
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- end card -->
+                <!-- end card body -->
+            </div>
+            <!-- end card -->
 
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">Poliçe PDf</h5>
-                    </div>
-                    <div class="card-body">
-                        <div>
-                            <p class="text-muted" id="polid">Poliçe Pdf Ekle.</p>
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Poliçe PDf</h5>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <p class="text-muted" id="polid">Poliçe Pdf Ekle.</p>
 
-                            <div class="dropzone" id="poldiv">
-                                <div class="fallback">
-                                    <input name="file" type="file" id="insurance_policy_url"
-                                        name="insurance_policy_url">
-                                </div>
-                                <div class="dz-message needsclick">
-                                    <div class="mb-3">
-                                        <i class="display-4 text-muted ri-upload-cloud-2-fill"></i>
-                                    </div>
-
-                                    <h5>Drop files here or click to upload.</h5>
-                                </div>
+                        <div class="dropzone" id="poldiv">
+                            <div class="fallback">
+                                <input name="file" type="file"  id="insurance_policy_url"  name="insurance_policy_url" >
                             </div>
+                            <div class="dz-message needsclick">
+                                <div class="mb-3">
+                                    <i class="display-4 text-muted ri-upload-cloud-2-fill"></i>
+                                </div>
 
-                            <ul class="list-unstyled mb-0" id="dropzone-preview">
-                                <li class="mt-2" id="dropzone-preview-list">
-                                    <!-- This is used as the file preview template -->
-                                    <div class="border rounded">
-                                        <div class="d-flex p-2">
-                                            <div class="flex-shrink-0 me-3">
-                                                <div class="avatar-sm bg-light rounded">
-                                                    <img src="https://mobiloby.app/koluman/web/upload/pdf.png"
-                                                        alt="Project-Image" data-dz-thumbnail
-                                                        class="img-fluid rounded d-block" />
-                                                </div>
-                                            </div>
-                                            <div class="flex-grow-1">
-                                                <div class="pt-1">
-                                                    <h5 class="fs-14 mb-1" data-dz-name>&nbsp;</h5>
-                                                    <p class="fs-13 text-muted mb-0" data-dz-size></p>
-                                                    <strong class="error text-danger" data-dz-errormessage></strong>
-                                                </div>
-                                            </div>
-                                            <div class="flex-shrink-0 ms-3">
-                                                <button data-dz-remove class="btn btn-sm btn-danger">Sil</button>
+                                <h5>Drop files here or click to upload.</h5>
+                            </div>
+                        </div>
+
+                        <ul class="list-unstyled mb-0" id="dropzone-preview">
+                            <li class="mt-2" id="dropzone-preview-list">
+                                <!-- This is used as the file preview template -->
+                                <div class="border rounded">
+                                    <div class="d-flex p-2">
+                                        <div class="flex-shrink-0 me-3">
+                                            <div class="avatar-sm bg-light rounded">
+                                                <img src="https://mobiloby.app/koluman/web/upload/pdf.png" alt="Project-Image" data-dz-thumbnail
+                                                    class="img-fluid rounded d-block" />
                                             </div>
                                         </div>
+                                        <div class="flex-grow-1">
+                                            <div class="pt-1">
+                                                <h5 class="fs-14 mb-1" data-dz-name>&nbsp;</h5>
+                                                <p class="fs-13 text-muted mb-0" data-dz-size></p>
+                                                <strong class="error text-danger" data-dz-errormessage></strong>
+                                            </div>
+                                        </div>
+                                        <div class="flex-shrink-0 ms-3">
+                                            <button data-dz-remove class="btn btn-sm btn-danger">Sil</button>
+                                        </div>
                                     </div>
-                                </li>
-                            </ul>
-                            <!-- end dropzon-preview -->
-                        </div>
+                                </div>
+                            </li>
+                        </ul>
+                        <!-- end dropzon-preview -->
                     </div>
                 </div>
-                <!-- end card -->
-                <div class="text-end mb-4">
-                    <button type="button" id="deleteallbutton" data-bs-toggle="modal" href="#deleteAll"
-                        class="btn btn-danger w-sm">Sil</button>
-                    <button type="button" id="newbutton" class="btn btn-success w-sm">Ekle</button>
-                </div>
-
             </div>
-            <!-- end col -->
-            <div class="col-lg-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">Durum</h5>
-                    </div>
-                    <div class="card-body">
-                        <div>
-                            <label for="choices-privacy-status-input" class="form-label">Durum</label>
-                            <select class="form-select" id="insurance_state" name="insurance_state">
-                                <option value="0">Lütfen Seçim Yapınız</option>
-                                <option value="1">Talep Oluştu</option>
-                                <option value="2">İncelemede</option>
-                                <option value="3">Teklif Oluştu</option>
-                                <option value="4">Aktif</option>
-                            </select>
-                        </div>
-                    </div>
-                    <!-- end card body -->
-                </div>
-                <!-- end card -->
-
-
-
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title mb-0">Kullanıcılar</h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="mb-3">
-                            <label for="choices-lead-input" class="form-label">Kullanıcı Listesi</label>
-                            <select class="form-select" id="user_id" name="user_id">
-                                <option value="0">Lütfen Seçim Yapınız</option>
-                                @foreach ($users as $u)
-                                    <option value="{{ $u->user_id }}">{{ $u->user_name . ' (' . $u->user_phone . ')' }}
-                                    </option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-                    <!-- end card body -->
-                </div>
-
-                <div class="text-end mb-4">
-                    <button type="button" id="updinc" class="btn btn-info w-sm">İncelemeye Al</button>
-                    <button type="button" id="updsnc" class="btn btn-secondary w-sm">Sonuçlandır</button>
-                </div>
-                <!-- end card -->
+            <!-- end card -->
+            <div class="text-end mb-4">
+                <button type="button" id="deleteallbutton" data-bs-toggle="modal" href="#deleteAll" class="btn btn-danger w-sm">Sil</button>
+                <button type="button" id="newbutton" class="btn btn-success w-sm">Ekle</button>
             </div>
-            <!-- end col -->
+
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Durum</h5>
+                </div>
+                <div class="card-body">
+                    <div>
+                        <label for="choices-privacy-status-input" class="form-label">Durum</label>
+                        <select class="form-select" id="insurance_state" name="insurance_state">
+                            <option value="0">Lütfen Seçim Yapınız</option>
+                            <option value="1">Talep Oluştu</option>
+                            <option value="2">İncelemede</option>
+                            <option value="3">Teklif Oluştu</option>
+                            <option value="4">Aktif</option>
+                        </select>
+                    </div>
+                </div>
+                <!-- end card body -->
+            </div>
+            <!-- end card -->
+
+
+
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title mb-0">Kullanıcılar</h5>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <label for="choices-lead-input" class="form-label">Kullanıcı Listesi</label>
+                        <select class="form-select" id="user_id" name="user_id">
+                             <option value="0">Lütfen Seçim Yapınız</option>
+                            @foreach ($users as $u)
+                                <option value="{{ $u->user_id }}">{{ $u->user_name . ' (' . $u->user_phone . ')' }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <!-- end card body -->
+            </div>
+
+            <div class="text-end mb-4">
+                <button type="button" id="updinc" class="btn btn-info w-sm">İncelemeye Al</button>
+                <button type="button" id="updsnc" class="btn btn-secondary w-sm">Sonuçlandır</button>
+            </div>
+            <!-- end card -->
+        </div>
         </form>
-
     </div>
     <!-- end row -->
     <div class="modal fade flip" id="deleteAll" tabindex="-1" aria-hidden="true">
