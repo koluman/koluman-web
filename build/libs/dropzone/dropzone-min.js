849b841757221f554a8e2496c12aacc7d0c3099a
MobilobyGit <info@mobiloby.com>
2024-01-31 11:27:43 +0300

    fghfhfgh

--- build/libs/dropzone/dropzone-min.js
+++ build/libs/dropzone/dropzone-min.js
@@ -360,6 +360,8 @@
             },
             removedfile: function (e) {
                 document.querySelector("#poldiv").style.display = "block";
+                $("#polid").text("");
+
                 return null != e.previewElement && null != e.previewElement.parentNode && e.previewElement.parentNode.removeChild(e.previewElement), this._updateMaxFilesReachedClass()
             },
             thumbnail: function (e, t) {
