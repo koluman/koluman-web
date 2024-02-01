1cdeb6d29ef4209e31e9c22cd4eecec18727fd67
MobilobyGit <info@mobiloby.com>
2024-02-01 15:47:35 +0300

    dfgfdg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                         <div class="text-center mt-sm-5 mb-4 text-white-50">
                             <div>
                                 <a href="index" class="d-inline-block auth-logo">
-                                    <img src="{{ URL::asset('build/images/logo-light.png') }}" alt=""
+                                    <img src="https://koluman.com.tr/assets/images/koluman-og.jpg" alt=""
                                         height="20">
                                 </a>
                             </div>
