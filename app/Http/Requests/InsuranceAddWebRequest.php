b56c2419eba49cff479a45201c99b4a1c272e9ef
MobilobyGit <info@mobiloby.com>
2024-02-01 12:27:50 +0300

    dfgfd

--- app/Http/Requests/InsuranceAddWebRequest.php
+++ app/Http/Requests/InsuranceAddWebRequest.php
@@ -12,7 +12,7 @@ class InsuranceAddWebRequest extends FormRequest
         throw new HttpResponseException(response()->json([
             'success' => 0,
             'message' => $validator->errors(),
-        ], 422));
+        ]));
     }
     public function authorize()
     {
