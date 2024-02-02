e141fbe6b93e226d0bd9c518f9e02ee4541e0805
MobilobyGit <info@mobiloby.com>
2024-02-02 11:44:09 +0300

    dfgfd

--- build/js/pages/calendar.init.js
+++ build/js/pages/calendar.init.js
@@ -196,6 +196,18 @@ document.addEventListener("DOMContentLoaded", function () {
                                             });
                                             eventCategoryChoice5.setChoiceByValue(selectedEvent.title);
                                         }
+                                        var choicesArray = [];
+                                        var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
+                                        for (var i = 0; i < allTimes.length; i++) {
+                                            var time = allTimes[i];
+                                            var choice = {
+                                                value: time,
+                                                label: time,
+                                            };
+                                            choicesArray.push(choice);
+                                        }
+                                        eventCategoryChoice5.clearChoices(); // Clear existing choices
+                                        eventCategoryChoice5.setChoices(choicesArray, 'value', 'label', true); // Set new choices
 
                                         var st_date = selectedEvent.start;
                                         var ed_date = selectedEvent.end;
--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -3,19 +3,6 @@ var eventCategoryChoice3; // Declare eventCategoryChoice3 at the top level
 document.addEventListener("DOMContentLoaded", function () {
     getshowroom();
     getapiusers();
-   
-    var choicesArray = [];
-    var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
-    for (var i = 0; i < allTimes.length; i++) {
-        var time = allTimes[i];
-        var choice = {
-            value: time,
-            label: time,
-        };
-        choicesArray.push(choice);
-    }
-    eventCategoryChoice5.clearChoices(); // Clear existing choices
-    eventCategoryChoice5.setChoices(choicesArray, 'value', 'label', true); // Set new choices
     $("#appointment_date").change(function () {
         getdate();
     });
