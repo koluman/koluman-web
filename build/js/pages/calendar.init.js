ae6ef5d83842b502bcc2b29915a6a2c8311fa80a
MobilobyGit <info@mobiloby.com>
2024-02-02 11:38:47 +0300

    fsdfsdf

--- build/js/pages/calendar.init.js
+++ build/js/pages/calendar.init.js
@@ -42,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                         id: event.appointment_id,
                                         title: event.appointment_time, // Yeni başlık
                                         start: new Date(event.appointment_date),
+                                        end: new Date(event.appointment_date), // İsterseniz aynı tarih olarak bırakabilirsiniz
                                         allDay: true,
                                         className: event.state == 0 ? 'bg-danger-subtle' : ' bg-success-subtle',
                                         location: {
@@ -49,9 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                             car_name: event.car_name
                                         },
                                         extendedProps: {
-                                            department: event.appointment_time,
-                                            appointment_date: event.appointment_date
-
+                                            department: event.appointment_time
                                         },
                                         description: {
                                             user_id: event.user_id,
@@ -64,6 +63,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                         id: event.appointment_id,
                                         title: event.appointment_time, // Yeni başlık
                                         start: new Date(event.appointment_date),
+                                        end: new Date(event.appointment_date), // İsterseniz aynı tarih olarak bırakabilirsiniz
                                         allDay: true,
                                         className: event.state == 0 ? 'bg-danger-subtle' : ' bg-success-subtle',
                                         location: event.car_name,
@@ -196,8 +196,9 @@ document.addEventListener("DOMContentLoaded", function () {
                                             });
                                             eventCategoryChoice4.setChoiceByValue(selectedEvent.title);
                                         }
-                                        console.log(selectedEvent);
+
                                         var st_date = selectedEvent.start;
+                                        var ed_date = selectedEvent.end;
                                         var date_r = function formatDate(date) {
                                             var d = new Date(date),
                                                 month = '' + (d.getMonth() + 1),
@@ -209,14 +210,19 @@ document.addEventListener("DOMContentLoaded", function () {
                                                 day = '0' + day;
                                             return [year, month, day].join('-');
                                         };
-                                       
-                                        var r_date = str_dt(st_date);
-                                        var er_date = date_r(st_date);
+                                        var updateDay = null
+                                        if (ed_date != null) {
+                                            var endUpdateDay = new Date(ed_date);
+                                            updateDay = endUpdateDay.setDate(endUpdateDay.getDate() - 1);
+                                        }
+                                        var r_date = ed_date == null ? (str_dt(st_date)) : (str_dt(st_date)) + ' to ' + (str_dt(updateDay));
+                                        var er_date = ed_date == null ? (date_r(st_date)) : (date_r(st_date)) + ' to ' + (date_r(updateDay));
                                         flatpickr(start_date, {
                                             defaultDate: er_date,
                                             altInput: true,
                                             altFormat: "j F Y",
                                             dateFormat: "Y-m-d",
+                                            mode: ed_date !== null ? "range" : "range",
                                         });
                                         document.getElementById("event-start-date-tag").innerHTML = r_date;
                                         newEventData = null;
