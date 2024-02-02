ebdafb296a6469af28090639ab3bff20bf2a1867
MobilobyGit <info@mobiloby.com>
2024-02-02 10:41:09 +0300

    fdgdfg

--- build/js/panel/testdrive.js
+++ build/js/panel/testdrive.js
@@ -29,28 +29,8 @@ function getdate() {
         dataType: 'json',
         success: function (data) {
             if (data.success == 1) {
-                /* var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
-                 // Tüm saatleri döngüye al ve dolu olanları işaretle
-                 var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
-
-                 for (var i = 0; i < allTimes.length; i++) {
-                     var time = allTimes[i];
-                     var isTimeOccupied = data.schedules.some(schedule => schedule.appointment_time === time);
-
-                     if (isTimeOccupied) {
-                         option += "<option value='" + time + "'>" + time + "</option>";
-                     } else {
-                         option += "<option value='" + time + "'>" + time + "</option>";
-                     }
-                 }
-                 console.log(option);
-                 $('#appointment_time').html('');
-                 $('#appointment_time').html(option);*/
-
                 var choicesArray = [];
-                // Tüm saatleri döngüye al ve dolu olanları işaretle
                 var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
-
                 for (var i = 0; i < allTimes.length; i++) {
                     var time = allTimes[i];
                     var isTimeOccupied = data.schedules.some(schedule => schedule.appointment_time === time);
@@ -63,14 +43,8 @@ function getdate() {
 
                     choicesArray.push(choice);
                 }
-                console.log(choicesArray);
                 eventCategoryChoice3.clearChoices(); // Clear existing choices
                 eventCategoryChoice3.setChoices(choicesArray, 'value', 'label', true); // Set new choices
-        
-
-
-
-
             }
 
         }
