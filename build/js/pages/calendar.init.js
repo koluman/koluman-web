f77ca839c9c48f6a20408bcde8648aa421e0ac66
MobilobyGit <info@mobiloby.com>
2024-02-02 12:22:04 +0300

    sdfdsf

--- build/js/pages/calendar.init.js
+++ build/js/pages/calendar.init.js
@@ -4,7 +4,6 @@ var T_check = null;
 var csrfToken = $('meta[name="csrf-token"]').attr('content');
 document.addEventListener("DOMContentLoaded", function () {
     testdrive();
-
     function testdrive() {
         //flatPickrInit();
         var addEvent = new bootstrap.Modal(document.getElementById('event-modal'), {
@@ -418,9 +417,95 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+    getshowroom();
+    getapiusers();
+    $("#appointment_date").change(function () {
+        getdate();
+    });
     
 });
+var csrfToken = $('meta[name="csrf-token"]').attr('content');
 
+function getdate() {
+    // Seçilen tarihi al
+    var selectedDate = $("#appointment_date").val();
+    var selectedCar = $("#car_id").val();
+    $.ajax({
+        type: 'POST',
+        url: 'https://mobiloby.app/koluman/web/testdriveschedules',
+        data: {
+            selectedDate: selectedDate,
+            selectedCar,
+            selectedCar,
+            _token: csrfToken, // CSRF token'ını gönder
+        },
+        dataType: 'json',
+        success: function (data) {
+            if (data.success == 1) {
+                var choicesArray = [];
+                var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
+                for (var i = 0; i < allTimes.length; i++) {
+                    var time = allTimes[i];
+                    var isTimeOccupied = data.schedules.some(schedule => schedule.appointment_time === time);
+
+                    var choice = {
+                        value: time,
+                        label: time,
+                        disabled: isTimeOccupied
+                    };
+
+                    choicesArray.push(choice);
+                }
+                eventCategoryChoice5.clearChoices(); // Clear existing choices
+                eventCategoryChoice5.setChoices(choicesArray, 'value', 'label', true); // Set new choices
+            }
+
+        }
+    });
+};
+
+function getshowroom() {
+    
+    $.ajax({
+        type: 'GET',
+        url: 'https://mobiloby.app/koluman/web/getshowroomcars',
+        dataType: 'json',
+        success: function (data) {
+            if (data.success == 1) {
+                var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
+                for (i = 0; i < data.showroomcars.length; ++i) {
+                    option += "<option value='" + data.showroomcars[i]["car_id"] + "'>" + data.showroomcars[i]["car_name"] + "</option>";
+
+                }
+                $('#car_id').html('');
+                $('#car_id').html(option);
+            }
+        }
+    });
+}
+
+function getapiusers() {
+    $.ajax({
+        type: 'POST',
+        url: 'https://mobiloby.app/koluman/web/getapiusers',
+        data: {
+            _token: csrfToken, // CSRF token'ını gönder
+        },
+        dataType: 'json',
+        success: function (data) {
+            if (data.success == 1) {
+                var option = "<option value='0'>Lütfen Seçim Yapınız</option>";
+                for (i = 0; i < data.usersall.length; ++i) {
+                    option += "<option value='" + data.usersall[i]["user_id"] + "'>" + data.usersall[i]["user_name"] + "</option>";
+                }
+                $('#user_id').html('');
+                $('#user_id').html(option);
+            }
+
+        }
+    });
+
+}
 function flatpicekrValueClear() {
     start_date.flatpickr().clear();
 }
