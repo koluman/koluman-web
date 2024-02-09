61d2f1375d9c18d0bbbd506f0bbe64a82702e099
MobilobyGit <info@mobiloby.com>
2024-02-09 17:49:05 +0300

    dfgfdg

--- resources/views/admin/testdrive.blade.php
+++ resources/views/admin/testdrive.blade.php
@@ -32,6 +32,10 @@
                                     data-class="bg-danger-subtle">
                                     <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Onaylanmayan Randevular
                                 </div>
+                                <div class="external-event fc-event bg-warning-subtle text-warning"
+                                    data-class="bg-warning-subtle">
+                                    <i class="mdi mdi-checkbox-blank-circle font-size-11 me-2"></i>Randevu Saati Geçmiş
+                                </div>
                             </div>
 
                         </div>
