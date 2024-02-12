326be826e7103ee1100d20fa34911ba7d4c578db
MobilobyGit <info@mobiloby.com>
2024-02-12 15:47:20 +0300

    dfsdf

--- resources/views/ajans/detail.blade.php
+++ resources/views/ajans/detail.blade.php
@@ -247,72 +247,7 @@
                 <!-- end card body -->
             </div>
             <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Publish Schedule</h5>
-                </div>
-                <!-- end card body -->
-                <div class="card-body">
-                    <div>
-                        <label for="datepicker-publish-input" class="form-label">Publish Date & Time</label>
-                        <input type="text" id="datepicker-publish-input" class="form-control"
-                            placeholder="Enter publish date" data-provider="flatpickr" data-date-format="d.m.y"
-                            data-enable-time>
-                    </div>
-                </div>
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Categories</h5>
-                </div>
-                <div class="card-body">
-                    <p class="text-muted mb-2"> <a href="#" class="float-end text-decoration-underline">Add
-                        New</a>Select product category</p>
-                        <select class="form-select" id="choices-category-input" name="choices-category-input" data-choices data-choices-search-false>
-                            <option value="Appliances">Appliances</option>
-                            <option value="Automotive Accessories">Automotive Accessories</option>
-                            <option value="Electronics">Electronics</option>
-                            <option value="Fashion">Fashion</option>
-                            <option value="Furniture">Furniture</option>
-                            <option value="Grocery">Grocery</option>
-                            <option value="Kids">Kids</option>
-                            <option value="Watches">Watches</option>
-                        </select>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Tags</h5>
-                </div>
-                <div class="card-body">
-                    <div class="hstack gap-3 align-items-start">
-                        <div class="flex-grow-1">
-                            <input class="form-control" data-choices data-choices-multiple-remove="true" placeholder="Enter tags" type="text"
-                        value="Cotton" />
-                        </div>
-                    </div>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="card-title mb-0">Product Short Description</h5>
-                </div>
-                <div class="card-body">
-                    <p class="text-muted mb-2">Add short description for product</p>
-                    <textarea class="form-control" placeholder="Must enter minimum of a 100 characters" rows="3"></textarea>
-                </div>
-                <!-- end card body -->
-            </div>
-            <!-- end card -->
-
+      
         </div>
     </div>
     <!-- end row -->
