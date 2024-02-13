11caf420980cdc6281f5f9e1b2e3fa6aca94be2a
MobilobyGit <info@mobiloby.com>
2024-02-13 14:12:56 +0300

    dfdfdsf

--- build/js/pages/calendar.init.js
+++ build/js/pages/calendar.init.js
@@ -24,21 +24,22 @@ document.addEventListener("DOMContentLoaded", function () {
     step2 = new Choices("#step2", {
         searchEnabled: false
     });
-    var choicesArray = [];
-    var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
-    for (var i = 0; i < allTimes.length; i++) {
-        var time = allTimes[i];
-        var choice = {
-            value: time,
-            label: time,
-        };
-        choicesArray.push(choice);
-    }
-    eventCategoryChoice5.clearChoices(); // Clear existing choices
-    eventCategoryChoice5.setChoices(choicesArray, 'value', 'label', true); // Set new choices
+   
     testdrive();
 
     function testdrive() {
+        var r = [];
+        var allTimes = ["09:00", "09:30", "10:00", "10:30", "11:00", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00"];
+        for (var i = 0; i < allTimes.length; i++) {
+            var time = allTimes[i];
+            var choice = {
+                value: time,
+                label: time,
+            };
+            r.push(choice);
+        }
+        eventCategoryChoice5.clearChoices(); // Clear existing choices
+        eventCategoryChoice5.setChoices(r, 'value', 'label', true); // Set new choices
         //flatPickrInit();
         var addEvent = new bootstrap.Modal(document.getElementById('event-modal'), {
             keyboard: false
