a51b3d61f3277f67e56f021ffa1f723e58acc5b8
MobilobyGit <info@mobiloby.com>
2024-02-14 14:41:37 +0300

    dfdf

--- build/js/pages/ecommerce-product-list.init.js
+++ build/js/pages/ecommerce-product-list.init.js
@@ -277,10 +277,37 @@ var filterDataAll = '';
 var filterDataPublished = '';
 
 
-var isFilterSelected = item.querySelectorAll(".filter-check .form-check .form-check-input:checked").length;
+// sidebar filter check
+Array.from(document.querySelectorAll(".filter-accordion .accordion-item")).forEach(function (item) {
+	var isFilterSelected = item.querySelectorAll(".filter-check .form-check .form-check-input:checked").length;
 	item.querySelector(".filter-badge").innerHTML = isFilterSelected;
+	Array.from(item.querySelectorAll(".form-check .form-check-input")).forEach(function (subitem) {
+		var checkElm = subitem.value;
+		if (subitem.checked) {
+			filterChoicesInput.setValue([checkElm]);
+		}
+		subitem.addEventListener("click", function (event) {
+			if (subitem.checked) {
+				isFilterSelected++;
+				item.querySelector(".filter-badge").innerHTML = isFilterSelected;
+				(isFilterSelected > 0) ? item.querySelector(".filter-badge").style.display = 'block' : item.querySelector(".filter-badge").style.display = 'none';
+				filterChoicesInput.setValue([checkElm]);
 
-
+			} else {
+				filterChoicesInput.removeActiveItemsByValue(checkElm);
+			}
+		});
+		filterChoicesInput.passedElement.element.addEventListener('removeItem', function (event) {
+			if (event.detail.value == checkElm) {
+				subitem.checked = false;
+				isFilterSelected--;
+				item.querySelector(".filter-badge").innerHTML = isFilterSelected;
+				(isFilterSelected > 0) ? item.querySelector(".filter-badge").style.display = 'block' : item.querySelector(".filter-badge").style.display = 'none';
+			}
+		}, false);
+	
+	});
+});
 
 
 
