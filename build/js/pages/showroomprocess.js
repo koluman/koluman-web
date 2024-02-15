a476e5854a6b0b81b38e6bd0d732bc1279ea1819
MobilobyGit <info@mobiloby.com>
2024-02-15 18:16:16 +0300

    sadasd

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -47,7 +47,50 @@ var id = getIdFromUrl();
 if (id != "" && id != null) getdetail(id);
 else add();
 
+function getcompany() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://mobiloby.app/koluman/web/getApiToken',
+        dataType: 'json',
+        success: function (data) {
 
+            if (data.success == 1) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://mobiloby.app/koluman/web/api/getcompanies',
+                    dataType: 'json',
+                    headers: {
+                        "Authorization": 'Bearer ' + data.token
+                    },
+                    success: function (data) {
+                        if (data.success == 1) {
+                            var ch = [];
+                            var v = "";
+                            var t = "Lütfen Seçiniz";
+                            var c = {
+                                value: v,
+                                label: t,
+                            };
+                            ch.push(c);
+                            for (var i = 0; i < data.companies.length; i++) {
+                                v = data.companies[i]["company_id"];
+                                t = data.companies[i]["company_name"];
+
+                                c = {
+                                    value: v,
+                                    label: t,
+                                };
+                                ch.push(c);
+                            }
+                            company.clearChoices(); // Clear existing choices
+                            company.setChoices(ch, 'value', 'label', true); // Set new choices
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
 function getIdFromUrl() {
     var url = window.location.href;
     var match = url.match(/\/showroomdetail\/(\d+)/);
@@ -133,50 +176,6 @@ function add() {
 
 }
 
-function getcompany() {
-    $.ajax({
-        type: 'GET',
-        url: 'https://mobiloby.app/koluman/web/getApiToken',
-        dataType: 'json',
-        success: function (data) {
-
-            if (data.success == 1) {
-                $.ajax({
-                    type: 'GET',
-                    url: 'https://mobiloby.app/koluman/web/api/getcompanies',
-                    dataType: 'json',
-                    headers: {
-                        "Authorization": 'Bearer ' + data.token
-                    },
-                    success: function (data) {
-                        if (data.success == 1) {
-                            var ch = [];
-                            var v = "";
-                            var t = "Lütfen Seçiniz";
-                            var c = {
-                                value: v,
-                                label: t,
-                            };
-                            ch.push(c);
-                            for (var i = 0; i < data.companies.length; i++) {
-                                v = data.companies[i]["company_id"];
-                                t = data.companies[i]["company_name"];
-
-                                c = {
-                                    value: v,
-                                    label: t,
-                                };
-                                ch.push(c);
-                            }
-                            company.clearChoices(); // Clear existing choices
-                            company.setChoices(ch, 'value', 'label', true); // Set new choices
-                        }
-                    }
-                });
-            }
-        }
-    });
-}
 $("#company_id").change(function () {
     getstep();
 });
