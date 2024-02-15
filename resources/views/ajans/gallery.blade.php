fab80697087eb9e5a4b2fef13fc0d5a63c37249a
MobilobyGit <info@mobiloby.com>
2024-02-15 11:32:32 +0300

    dfdsf

--- resources/views/ajans/gallery.blade.php
+++ resources/views/ajans/gallery.blade.php
@@ -14,131 +14,184 @@
             Gallery
         @endslot
     @endcomponent
-   <div class="row">
-        <div class="col-xl-3 col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <div class="d-flex mb-3">
-                        <div class="flex-grow-1">
-                            <h5 class="fs-16">Filtreler</h5>
-                        </div>
-                        <div class="flex-shrink-0">
-                            <a href="#" class="text-decoration-underline" id="clearall">Tümünü Temizle</a>
-                        </div>
-                    </div>
-                </div>
+<form id="createproduct-form" autocomplete="off" class="needs-validation" novalidate>
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <div class="mb-3">
+                                <label class="form-label" for="product-title-input">Kategori</label>
+                                <input type="hidden" class="form-control" id="formAction" name="formAction" value="add">
+                                <input type="text" class="form-control d-none" id="product-id-input">
+                                <input type="text" class="form-control" id="product-title-input" value=""
+                                    required>
+                                <div class="invalid-feedback">Please Enter a product title.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="product-title-input">Araba Adı</label>
+                                <input type="hidden" class="form-control" id="formAction" name="formAction" value="add">
+                                <input type="text" class="form-control d-none" id="product-id-input">
+                                <input type="text" class="form-control" id="product-title-input" value=""
+                                   required>
+                                <div class="invalid-feedback">Please Enter a product title.</div>
+                            </div>
 
-                <div class="accordion accordion-flush filter-accordion">
-
-                    <div class="card-body border-bottom">
+                        </div>
                         <div>
-                            <p class="text-muted text-uppercase fs-12 fw-medium mb-2">Kategoriler</p>
-                            <ul class="list-unstyled mb-0 filter-list">
-                            @foreach ($companies as $c)
-
-                               <li>
-                                    <a href="#" class="d-flex py-1 align-items-center">
-                                        <div class="flex-grow-1">
-                                            <h5 class="fs-13 mb-0 listname">{{$c->company_name}}</h5>
-                                        </div>
-                                        <div class="flex-shrink-0 ms-2">
-                                            <span class="badge bg-light text-muted">{{$c->carCount}}</span>
-                                        </div>
-                                    </a>
-                                </li>
-                            @endforeach 
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="accordion-item">
-                        <h2 class="accordion-header" id="flush-headingBrands">
-                            <button class="accordion-button bg-transparent shadow-none" type="button"
-                                data-bs-toggle="collapse" data-bs-target="#flush-collapseBrands"
-                                aria-expanded="true" aria-controls="flush-collapseBrands">
-                                <span class="text-muted text-uppercase fs-12 fw-medium">Kategori</span> <span
-                                    class="badge bg-success rounded-pill align-middle ms-1 filter-badge" id="adet"></span>
-                            </button>
-                        </h2>
-
-                        <div id="flush-collapseBrands" class="accordion-collapse collapse show"
-                            aria-labelledby="flush-headingBrands">
-                            <div class="accordion-body text-body pt-0">
-                                <div class="search-box search-box-sm">
-                                    <input type="text" class="form-control bg-light border-0" id="searchBrandsList" placeholder="Kategori Ara...">
-                                    <i class="ri-search-line search-icon"></i>
-                                </div>
-                                <div class="d-flex flex-column gap-2 mt-3 filter-check" id="tik">
-                                  
-
-                                    
-                                </div>
+                            <label>Araba Açıklama</label>
+                            <div id="ckeditor-classic">                               
                             </div>
                         </div>
                     </div>
-                    <!-- end accordion-item -->
+                </div>
+                <!-- end card -->
 
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Araba Kapak Resmi</h5>
+                    </div>
+                    <div class="card-body">
+                        <div>
+                            <p class="text-muted">Araba resişmleri ekleme formu.</p>
+
+                            <div class="dropzone">
+                                <div class="fallback">
+                                    <input name="file" type="file">
+                                </div>
+                                <div class="dz-message needsclick">
+                                    <div class="mb-3">
+                                        <i class="display-4 text-muted ri-upload-cloud-2-fill"></i>
+                                    </div>
+
+                                    <h5>Resmi buraya yükleyiniz.</h5>
+                                </div>
+                            </div>
+
+                            <ul class="list-unstyled mb-0" id="dropzone-preview">
+                                <li class="mt-2" id="dropzone-preview-list">
+                                    <div class="border rounded">
+                                        <div class="d-flex p-2">
+                                            <div class="flex-shrink-0 me-3">
+                                                <div class="avatar-sm bg-light rounded">
+                                                    <img data-dz-thumbnail class="img-fluid rounded d-block" src="#"
+                                                        alt="Product-Image" />
+                                                </div>
+                                            </div>
+                                            <div class="flex-grow-1">
+                                                <div class="pt-1">
+                                                    <h5 class="fs-14 mb-1" data-dz-name>&nbsp;</h5>
+                                                    <p class="fs-13 text-muted mb-0" data-dz-size></p>
+                                                    <strong class="error text-danger" data-dz-errormessage></strong>
+                                                </div>
+                                            </div>
+                                            <div class="flex-shrink-0 ms-3">
+                                                <button data-dz-remove class="btn btn-sm btn-danger">Sil</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                            <!-- end dropzon-preview -->
+                        </div>
+                    </div>
+                </div>
+                <!-- end card -->
+
+                <div class="text-end mb-3">
+                    <button type="submit" class="btn btn-success w-sm">Ekle</button>
                 </div>
             </div>
-            <!-- end card -->
-        </div>
-        <!-- end col -->
+            <!-- end col -->
 
-        <div class="col-xl-9 col-lg-8">
-            <div>
+            <div class="col-lg-4">
                 <div class="card">
-                    <div class="card-header border-0">
-                        <div class="row g-4">
-                            <div class="col-sm-auto">
-                                <div>
-                                    <a href="shoowroomdetail" class="btn btn-success" id="addproduct-btn"><i
-                                            class="ri-add-line align-bottom me-1"></i>Araba Ekle</a>
-                                </div>
-                            </div>
-                            <div class="col-sm">
-                                <div class="d-flex justify-content-sm-end">
-                                    <div class="search-box ms-2">
-                                        <input type="text" class="form-control" id="searchProductList" placeholder="Ara...">
-                                        <i class="ri-search-line search-icon"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                     <div class="card-header">
-                        <div class="row align-items-center">
-                           
-                            <div class="col-auto">
-                                <div id="selection-element">
-                                    <div class="my-n1 d-flex align-items-center text-muted">
-                                        Select <div id="select-content" class="text-body fw-semibold px-1"></div> Result <button type="button" class="btn btn-link link-danger p-0 ms-3 material-shadow-none" data-bs-toggle="modal" data-bs-target="#removeItemModal">Remove</button>
+                        <h5 class="card-title mb-0">Detay</h5>
+                    </div>
+                    <div class="card-body">
+                        <div class="live-preview">
+                            <div>
+                                <p class="text-muted">Açıklama yazılabilir</p>
+                                <div class="row g-3">
+                                    <div class="col-lg-12">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                aria-label="Text input with dropdown button">
+                                            <button class="btn btn-success dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">Seçiniz</button>
+                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                aria-label="Text input with dropdown button">
+                                            <button class="btn btn-success dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">Seçiniz</button>
+                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                aria-label="Text input with dropdown button">
+                                            <button class="btn btn-success dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">Seçiniz</button>
+                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                aria-label="Text input with dropdown button">
+                                            <button class="btn btn-success dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">Seçiniz</button>
+                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="input-group">
+                                            <input type="text" class="form-control"
+                                                aria-label="Text input with dropdown button">
+                                            <button class="btn btn-success dropdown-toggle" type="button"
+                                                data-bs-toggle="dropdown" aria-expanded="false">Seçiniz</button>
+                                            <ul class="dropdown-menu dropdown-menu-end">
+                                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- end card header -->
-                    <div class="card-body">
-
-                        <div class="tab-content text-muted">
-                            <div class="tab-pane active" id="productnav-all" role="tabpanel">
-                                <div id="table-product-list-all" class="table-card gridjs-border-none"></div>
-                            </div>
-                            <!-- end tab pane -->
-
-                        </div>
-                        <!-- end tab content -->
-
                     </div>
                     <!-- end card body -->
                 </div>
                 <!-- end card -->
+                <div class="card">
+                    <div class="card-header align-items-center d-flex">
+                    <h4 class="card-title mb-0 flex-grow-1">Test Sürüş Arabası</h4>
+                    <div class="flex-shrink-0">
+                        <div class="form-check form-switch form-switch-right form-switch-md">
+                            <label for="input-group-custom-showcode" class="form-label text-muted"></label>
+                            <input class="form-check-input code-switcher" type="checkbox" id="input-group-custom-showcode">
+                        </div>
+                    </div>
+                </div>
+                    <!-- end card body -->
+                </div>
             </div>
         </div>
-        <!-- end col -->
-    </div>
-    <!-- end row -->
+        <!-- end row -->
+    </form>
 @endsection
 @section('script')
     <script src="{{ URL::asset('build/libs/glightbox/js/glightbox.min.js') }}"></script>
