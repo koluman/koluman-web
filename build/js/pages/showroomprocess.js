e92ccc3eb7c7740c74112a4c2d3ab807aba73ec0
MobilobyGit <info@mobiloby.com>
2024-02-15 12:35:00 +0300

    dsfsdf

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -98,7 +98,7 @@ function getstep() {
         },
         dataType: 'json',
         success: function (data) {
-            console.log(data);
+            console.log($("#company_id").val());
 
             if (data.success == 1) {
                 let a="";
