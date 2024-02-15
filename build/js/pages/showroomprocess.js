47d814cbedbff8cc7c22ae4294f0cd100ed188d8
MobilobyGit <info@mobiloby.com>
2024-02-15 12:04:25 +0300

    dsffd

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -29,58 +29,54 @@ var dropzone = new Dropzone(".dropzone", {
     previewsContainer: "#dropzone-preview2",
 });
 
-// Form Event
-(function () {
-    'use strict'
-    let csrfToken = $('meta[name="csrf-token"]').attr('content');
-    let  company = new Choices("#company_id", {
-        searchEnabled: false
-    });
-    getcompany();
-    
-    function getcompany() {
-        $.ajax({
-            type: 'GET',
-            url: 'https://mobiloby.app/koluman/web/getApiToken',
-            dataType: 'json',
-            success: function (data) {
 
-                if (data.success == 1) {
-                    $.ajax({
-                        type: 'GET',
-                        url: 'https://mobiloby.app/koluman/web/api/getcompanies',
-                        dataType: 'json',
-                        headers: {
-                            "Authorization": 'Bearer ' + data.token
-                        },
-                        success: function (data) {
-                            if (data.success == 1) {
-                                var ch = [];
-                                var v = "";
-                                var t = "Lütfen Seçiniz";
-                                var c = {
+let csrfToken = $('meta[name="csrf-token"]').attr('content');
+let company = new Choices("#company_id", {
+    searchEnabled: false
+});
+getcompany();
+
+function getcompany() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://mobiloby.app/koluman/web/getApiToken',
+        dataType: 'json',
+        success: function (data) {
+
+            if (data.success == 1) {
+                $.ajax({
+                    type: 'GET',
+                    url: 'https://mobiloby.app/koluman/web/api/getcompanies',
+                    dataType: 'json',
+                    headers: {
+                        "Authorization": 'Bearer ' + data.token
+                    },
+                    success: function (data) {
+                        if (data.success == 1) {
+                            var ch = [];
+                            var v = "";
+                            var t = "Lütfen Seçiniz";
+                            var c = {
+                                value: v,
+                                label: t,
+                            };
+                            ch.push(c);
+                            for (var i = 0; i < data.companies.length; i++) {
+                                v = data.companies[i]["company_id"];
+                                t = data.companies[i]["company_name"];
+
+                                c = {
                                     value: v,
                                     label: t,
                                 };
                                 ch.push(c);
-                                for (var i = 0; i < data.companies.length; i++) {
-                                    v = data.companies[i]["company_id"];
-                                    t = data.companies[i]["company_name"];
-
-                                    c = {
-                                        value: v,
-                                        label: t,
-                                    };
-                                    ch.push(c);
-                                }
-                                company.clearChoices(); // Clear existing choices
-                                company.setChoices(ch, 'value', 'label', true); // Set new choices
                             }
+                            company.clearChoices(); // Clear existing choices
+                            company.setChoices(ch, 'value', 'label', true); // Set new choices
                         }
-                    });
-                }
+                    }
+                });
             }
-        });
-    }
-
-})()
+        }
+    });
+}
