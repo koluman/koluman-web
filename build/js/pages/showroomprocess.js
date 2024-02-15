ad1141a820e7c658e00fba64a624116f237d7b3b
MobilobyGit <info@mobiloby.com>
2024-02-15 14:47:48 +0300

    df

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -153,10 +153,8 @@ $("#addcar").click(function () {
     formData.append('step5', $("#step5text").val());
     formData.append('state',document.querySelector("#state").checked);
 
-    document.querySelector('#ckeditor-classic').on('change:data', function () {
         var editorContent = document.querySelector('#ckeditor-classic').getData();
         console.log(editorContent);
-    });
     
 
    /* if ($("#car_id").val() != "") $url = "https://mobiloby.app/koluman/web/updateshowroom";
