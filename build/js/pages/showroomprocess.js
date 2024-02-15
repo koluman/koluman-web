1c6a3a77ea0384ac22b7134091c634246d713dba
MobilobyGit <info@mobiloby.com>
2024-02-15 12:48:03 +0300

    dfgfdg

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -121,6 +121,7 @@ function getstep1(stp) {
     var filteredSteps = steps.filter(item => item.step1 === stp);
     var uniqueStep2Values = [...new Set(filteredSteps.map(item => item.step2))];
     let b="";
+    console.log(uniqueStep2Values);
     for (var i = 0; i < uniqueStep2Values.length; i++) {
         b+='<li><a class="dropdown-item" href="javascript:getstep3(\'' + uniqueStep2Values[i]["step2"] + '\')">'+uniqueStep2Values[i]["step2"]+'</a></li>';
     }
