b2175da92fd1090cbacb455410a2d7f6e4691260
MobilobyGit <info@mobiloby.com>
2024-02-15 11:27:17 +0300

    dfgfdg

--- resources/views/ajans/gallery.blade.php
+++ resources/views/ajans/gallery.blade.php
@@ -451,7 +451,7 @@
     <script src="{{ URL::asset('build/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js') }}"></script>
 
     <script src="{{ URL::asset('build/libs/dropzone/dropzone-min.js') }}"></script>
-    <script src="{{ URL::asset('build/js/pages/ecommerce-product-create.init.js') }}"></script>
+    <script src="{{ URL::asset('build/js/pages/gallery-create.js') }}"></script>
 
     <script src="{{ URL::asset('build/js/app.js') }}"></script>
 @endsection
