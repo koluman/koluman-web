7fc35cdf5d3b36af1864e0360f0bac1d9230fbff
MobilobyGit <info@mobiloby.com>
2024-02-15 11:28:24 +0300

    dsfsf

--- build/js/pages/gallery-create.js
+++ build/js/pages/gallery-create.js
@@ -52,11 +52,7 @@ var dropzone = new Dropzone(".dropzone", {
     
     
 
-    // choices category input
-    var prdoctCategoryInput = new Choices('#choices-category-input', {
-        searchEnabled: false,
-    });
-    
+  
     var editinputValueJson = sessionStorage.getItem('editInputValue');
     if (editinputValueJson) {
         var editinputValueJson = JSON.parse(editinputValueJson);
