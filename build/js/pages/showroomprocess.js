682d774ea47f5fbc63b22965b390d9153b1f8a5c
MobilobyGit <info@mobiloby.com>
2024-02-16 14:26:28 +0300

    fsdfsd

--- build/js/pages/showroomprocess.js
+++ build/js/pages/showroomprocess.js
@@ -1,3 +1,4 @@
+
 let csrfToken = $('meta[name="csrf-token"]').attr('content');
 let company;
 let steps = [];
@@ -7,47 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     company = new Choices("#company_id", {
         searchEnabled: false
     });
-    $.ajax({
-        type: 'GET',
-        url: 'https://mobiloby.app/koluman/web/getApiToken',
-        dataType: 'json',
-        success: function (data) {
-            if (data.success == 1) {
-                $.ajax({
-                    type: 'GET',
-                    url: 'https://mobiloby.app/koluman/web/api/getcompanies',
-                    dataType: 'json',
-                    headers: {
-                        "Authorization": 'Bearer ' + data.token
-                    },
-                    success: function (data) {
-                        if (data.success == 1) {
-                            var ch = [];
-                            var v = "";
-                            var t = "Lütfen Seçiniz";
-                            var c = {
-                                value: v,
-                                label: t,
-                            };
-                            ch.push(c);
-                            for (var i = 0; i < data.companies.length; i++) {
-                                v = data.companies[i]["company_id"];
-                                t = data.companies[i]["company_name"];
-
-                                c = {
-                                    value: v,
-                                    label: t,
-                                };
-                                ch.push(c);
-                            }
-                            company.clearChoices(); // Clear existing choices
-                            company.setChoices(ch, 'value', 'label', true); // Set new choices
-                        }
-                    }
-                });
-            }
-        }
-    });
+    getcompany();
     var id = getIdFromUrl();
     if (id != "" && id != null) getdetail(id);
     else add();
@@ -83,7 +44,6 @@ if (dropzonePreviewNode) {
         }
     });
 }
-
 function getcompany() {
     $.ajax({
         type: 'GET',
@@ -207,7 +167,8 @@ function getdetail(id) {
                         document.querySelector("#step4text").disabled = false;
                         document.querySelector("#step5text").disabled = false;
 
-                    } else {
+                    }
+                    else{
                         $("#step1").html('');
                     }
                 }
@@ -282,9 +243,7 @@ $("#addcar").click(function () {
 });
 
 function getstep() {
-    let steps = [];
-    let uniqueValues = [];
-    var tt = [];
+    let steps = [];let uniqueValues = [];var tt = [];
     document.querySelector("#step1text").value = "";
     document.querySelector("#step2text").value = "";
     document.querySelector("#step3text").value = "";
@@ -313,7 +272,8 @@ function getstep() {
                 document.querySelector("#step1text").disabled = false;
                 $("#step1").html('');
                 $("#step1").html(a);
-            } else {
+            }
+            else{
                 $("#step1").html('');
             }
         }
