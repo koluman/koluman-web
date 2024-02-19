98eea5fd57f693bacf7aefa5d99a084bafbe467d
MobilobyGit <info@mobiloby.com>
2024-02-19 10:02:52 +0300

    dsfgdf

--- app/Http/Controllers/Mobil/ShoowroomController.php
+++ app/Http/Controllers/Mobil/ShoowroomController.php
@@ -37,26 +37,28 @@ class ShoowroomController extends Controller
     {
         try {
             $shoowroomdetail = Showroom::select(
-                'showroom.car_id',
-                'showroom.company_id',
-                'showroom.step1',
-                'showroom.step2',
-                'showroom.step3',
-                'showroom.step4',
-                'showroom.step5',
-                'showroom.car_name',
-                'showroom.car_description',
-                'showroom.car_image_url',
-                'showroom.isTestdrive',
-                'companies.company_name',
-                'showroom_gallery.gallery_id',
-                'showroom_gallery.car_img_url'
-            )
-                ->join('companies', 'showroom.company_id', '=', 'companies.company_id')
-                ->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
-                ->where('showroom.car_id', $request->car_id)
-                ->get();
-
+                    'showroom.car_id',
+                    'showroom.company_id',
+                    'showroom.step1',
+                    'showroom.step2',
+                    'showroom.step3',
+                    'showroom.step4',
+                    'showroom.step5',
+                    'showroom.car_name',
+                    'showroom.car_description',
+                    'showroom.car_image_url',
+                    'showroom.isTestdrive',
+                    'companies.company_name',
+                    'showroom_gallery.gallery_id',
+                    'showroom_gallery.created_at',
+                    'showroom_gallery.updated_at',
+                    'showroom_gallery.car_img_url'
+                )
+                    ->join('companies', 'showroom.company_id', '=', 'companies.company_id')
+                    ->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
+                    ->where('showroom.car_id', $request->car_id)
+                    ->get();
+                
 
             if (!$shoowroomdetail->isEmpty()) {
                 $responseData = [
