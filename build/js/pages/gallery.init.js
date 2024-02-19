aabe22058309e3f3b7c0033e909f5c80249544a3
MobilobyGit <info@mobiloby.com>
2024-02-19 13:40:56 +0300

    aSAD

--- build/js/pages/gallery.init.js
+++ build/js/pages/gallery.init.js
@@ -89,10 +89,43 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     success: function (data) {
                         console.log(data);
                         if(data.success==1){
+                            let veri=shoowroomdetail.gallery;let s="";
+                            for (let i = 0; i < veri.length; i++) {
+                               s+='    <div class="element-item col-xxl-3 col-xl-4 col-sm-6 project designing development" data-category="designing development">';
+                               s+='    <div class="gallery-box card">';
+                               s+='    <div class="gallery-container">';
+                               s+='        <a class="image-popup" href="{{ URL::asset("build/images/small/img-1.jpg") }}" title="">';
+                               s+='            <img class="gallery-img img-fluid mx-auto" src="{{ URL::asset("build/images/small/img-1.jpg") }}" alt="" />';
+                               s+='            <div class="gallery-overlay">';
+                               s+='                <h5 class="overlay-caption">Glasses and laptop from above</h5>';
+                               s+='            </div>';
+                               s+='        </a>';
+                               s+='    </div>';
+                               s+='        <div class="box-content">';
+                               s+='            <div class="d-flex align-items-center mt-1">';
+                               s+='                <div class="flex-grow-1 text-muted">by <a href="" class="text-body text-truncate">Ron Mackie</a></div>';
+                               s+='                <div class="flex-shrink-0">';
+                               s+='                    <div class="d-flex gap-3">';
+                               s+='                        <button type="button" class="btn btn-sm fs-12 btn-link text-body text-decoration-none px-0 material-shadow-none">';
+                               s+='                            <i class="ri-thumb-up-fill text-muted align-bottom me-1"></i> 2.2K';
+                               s+='                        </button>';
+                               s+='                        <button type="button" class="btn btn-sm fs-12 btn-link text-body text-decoration-none px-0 material-shadow-none">';
+                               s+='                            <i class="ri-question-answer-fill text-muted align-bottom me-1"></i> 1.3K';
+                               s+='                        </button>';
+                               s+='                    </div>';
+                               s+='                </div>';
+                               s+='            </div>';
+                               s+='        </div>';
+                               s+='    </div>';
+                               s+='</div>      ';
+                                
+                            }
+                            $("#gallery").html('');
+                            $("#gallery").html(s);
 
                         }
                         else{
-                            
+
                         }
                     }
 
