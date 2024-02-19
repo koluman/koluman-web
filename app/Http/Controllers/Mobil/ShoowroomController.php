b22a0b860d7a2296016ae63f33d201abc1cf82a3
MobilobyGit <info@mobiloby.com>
2024-02-19 10:05:52 +0300

    fghfgh

--- app/Http/Controllers/Mobil/ShoowroomController.php
+++ app/Http/Controllers/Mobil/ShoowroomController.php
@@ -37,33 +37,63 @@ class ShoowroomController extends Controller
     {
         try {
             $shoowroomdetail = Showroom::select(
-                    'showroom.car_id',
-                    'showroom.company_id',
-                    'showroom.step1',
-                    'showroom.step2',
-                    'showroom.step3',
-                    'showroom.step4',
-                    'showroom.step5',
-                    'showroom.car_name',
-                    'showroom.car_description',
-                    'showroom.car_image_url',
-                    'showroom.isTestdrive',
-                    'companies.company_name',
-                    'showroom_gallery.gallery_id',
-                    'showroom_gallery.created_at',
-                    'showroom_gallery.updated_at',
-                    'showroom_gallery.car_img_url'
-                )
-                    ->join('companies', 'showroom.company_id', '=', 'companies.company_id')
-                    ->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
-                    ->where('showroom.car_id', $request->car_id)
-                    ->get();
-                
+                'showroom.car_id',
+                'showroom.company_id',
+                'showroom.step1',
+                'showroom.step2',
+                'showroom.step3',
+                'showroom.step4',
+                'showroom.step5',
+                'showroom.car_name',
+                'showroom.car_description',
+                'showroom.car_image_url',
+                'showroom.isTestdrive',
+                'companies.company_name',
+                'showroom_gallery.gallery_id',
+                'showroom_gallery.created_at',
+                'showroom_gallery.updated_at',
+                'showroom_gallery.car_img_url'
+            )
+                ->join('companies', 'showroom.company_id', '=', 'companies.company_id')
+                ->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
+                ->where('showroom.car_id', $request->car_id)
+                ->get();
+            
+            // Gruplandırma işlemi
+            $groupedShowroomDetail = $shoowroomdetail->groupBy('car_id')->map(function ($group) {
+                $galleryImages = $group->map(function ($item) {
+                    return [
+                        'gallery_id' => $item->gallery_id,
+                        'created_at' => $item->created_at,
+                        'updated_at' => $item->updated_at,
+                        'car_img_url' => $item->car_img_url,
+                    ];
+                })->toArray();
+            
+                return [
+                    'car_id' => $group[0]->car_id,
+                    'company_id' => $group[0]->company_id,
+                    'step1' => $group[0]->step1,
+                    'step2' => $group[0]->step2,
+                    'step3' => $group[0]->step3,
+                    'step4' => $group[0]->step4,
+                    'step5' => $group[0]->step5,
+                    'car_name' => $group[0]->car_name,
+                    'car_description' => $group[0]->car_description,
+                    'car_image_url' => $group[0]->car_image_url,
+                    'isTestdrive' => $group[0]->isTestdrive,
+                    'company_name' => $group[0]->company_name,
+                    'gallery' => $galleryImages,
+                ];
+            })->values()->toArray();
+        
+            
+            
 
             if (!$shoowroomdetail->isEmpty()) {
                 $responseData = [
                     "success" => 1,
-                    "shoowroomdetail" => $shoowroomdetail,
+                    "shoowroomdetail" => $groupedShowroomDetail,
                     "message" => "Araba detay bilgileri getirildi",
                 ];
             } else {
