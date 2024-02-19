9ebed492d52b9e5b9f3ccb94d2ed9d78a25bb2c0
MobilobyGit <info@mobiloby.com>
2024-02-19 14:08:32 +0300

    sfdfdsfds

--- app/Http/Controllers/Ajans/AjansHomeController.php
+++ app/Http/Controllers/Ajans/AjansHomeController.php
@@ -15,37 +15,14 @@ class AjansHomeController extends Controller
     public function gallery(Request $request, $id = null)
     {
         $shoowroomdetail = Showroom::select(
-            'showroom.car_id',
-            'showroom.company_id',
-            'showroom.step1',
-            'showroom.step2',
-            'showroom.step3',
-            'showroom.step4',
-            'showroom.step5',
-            'showroom.car_name',
-            'showroom.car_description',
-            'showroom.car_image_url',
-            'showroom.isTestdrive',
-            'companies.company_name',
             'showroom_gallery.gallery_id',
-            'showroom_gallery.created_at',
             'showroom_gallery.updated_at',
             'showroom_gallery.car_img_url'
-        )
-            ->join('companies', 'showroom.company_id', '=', 'companies.company_id')
-            ->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
-            ->where('showroom.car_id', $request->id)
-            ->get();
-
-            $ShowroomDetail = (object)[
-                'gallery' => $shoowroomdetail->map(function ($item) {
-                    return [
-                        'gallery_id' => $item->gallery_id,
-                        'car_img_url' => $item->car_img_url,
-                    ];
-                })->toArray(),
-            ];
-       
-        return view('ajans.gallery',compact('ShowroomDetail'));
+        )->leftJoin('showroom_gallery', 'showroom.car_id', '=', 'showroom_gallery.car_id')
+        ->where('showroom.car_id', $request->id)
+        ->get();
+      
+       dd($shoowroomdetail);
+        //return view('ajans.gallery',compact('ShowroomDetail'));
     }
 }
