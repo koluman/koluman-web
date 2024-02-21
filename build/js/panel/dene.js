a3502c63da0a0b663c2ac1b27d33fd52094fd193
MobilobyGit <info@mobiloby.com>
2024-02-21 15:41:06 +0300

    dfssdf

---
+++ build/js/panel/dene.js
@@ -0,0 +1,32 @@
+itemid = 13;
+ClassicEditor
+    .create(document.querySelector('#ckeditor-classic'))
+    .then(function (editor) {
+        editor.ui.view.editable.element.style.height = '200px';
+        editor.model.document.on('change:data', function () {
+            var editorContent = editor.getData();
+            document.querySelector("#car_description").value = editorContent;
+        });
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+var dropzonePreviewNode = document.querySelector("#dropzone-preview2-list");
+var car_img_url;
+if (dropzonePreviewNode) {
+    dropzonePreviewNode.id = "";
+    var previewTemplate = dropzonePreviewNode.parentNode.innerHTML;
+    dropzonePreviewNode.parentNode.removeChild(dropzonePreviewNode);
+
+    var dropzone = new Dropzone(".dropzone", {
+        url: 'https://httpbin.org/post',
+        method: "post",
+        previewTemplate: previewTemplate,
+        previewsContainer: "#dropzone-preview2",
+        init: function () {
+            this.on("addedfile", function (file) {
+                car_img_url = file;
+            });
+        }
+    });
+}
