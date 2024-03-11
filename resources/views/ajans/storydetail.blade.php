a27a5d86ea978238a1e57c60698909fe6933aba9
MobilobyGit <info@mobiloby.com>
2024-03-11 16:56:07 +0300

    fdsf

--- resources/views/ajans/storydetail.blade.php
+++ resources/views/ajans/storydetail.blade.php
@@ -121,7 +121,7 @@
 
                         <div class="card">
                             <div class="card-header">
-                                <h5 class="card-title mb-0">Küçük Resim</h5>
+                                <h5 class="card-title mb-0">Büyük Resim</h5>
                             </div>
                             <div class="card-body">
                                 <div>
