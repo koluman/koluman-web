8c854c78b0303322dc1c64668edea7e186b70c01
MobilobyGit <info@mobiloby.com>
2024-03-11 17:34:42 +0300

    dsfdsf

--- build/js/panel/storyprocess.js
+++ build/js/panel/storyprocess.js
@@ -97,6 +97,7 @@ function getdetail(id) {
             _token: csrfToken, 
         },
         success: function (data) {
+            console.log(data.storyid[0].story_id);
             $("#story_id").val(data.storyid[0].story_id);
             $("#story_title").val(data.storyid[0].story_title);
             $("#company_id").val(data.storyid[0].company_id);
