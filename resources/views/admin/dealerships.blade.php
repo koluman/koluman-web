6c996b03f0e89bc98592a155d5e17eb664e6acb2
MobilobyGit <info@mobiloby.com>
2024-03-12 14:51:18 +0300

    dfgfg

--- resources/views/admin/dealerships.blade.php
+++ resources/views/admin/dealerships.blade.php
@@ -162,7 +162,7 @@
                                                         <div class="position-absolute bottom-0 end-0">
                                                             <label for="company-logo-input" class="mb-0"
                                                                 data-bs-toggle="tooltip" data-bs-placement="right"
-                                                                title="Select Image">
+                                                                title="Resim Seç">
                                                                 <div class="avatar-xs cursor-pointer">
                                                                     <div
                                                                         class="avatar-title bg-light border rounded-circle text-muted">
@@ -182,27 +182,25 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    <h5 class="fs-13 mt-3">Company Logo</h5>
+                                                    <h5 class="fs-13 mt-3">Şube Resmi</h5>
                                                 </div>
                                                 <div>
-                                                    <label for="companyname-field" class="form-label">Name</label>
+                                                    <label for="companyname-field" class="form-label">Şube Adı</label>
                                                     <input type="text" id="companyname-field" class="form-control"
                                                         placeholder="Enter company name" required />
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div>
-                                                    <label for="owner-field" class="form-label">Owner
-                                                        Name</label>
-                                                    <input type="text" id="owner-field" class="form-control"
+                                                    <label for="industry_type-field" class="form-label">Lat</label>
+                                                    <input type="text" id="industry_type-field" class="form-control"
                                                         placeholder="Enter owner name" required />
                                                 </div>
                                             </div>
                                             <div class="col-lg-6">
                                                 <div>
-                                                    <label for="industry_type-field" class="form-label">Industry
-                                                        Type</label>
-                                                    <select class="form-select" id="industry_type-field">
+                                                    <label for="owner-field" class="form-label">Şehir</label>
+                                                    <select class="form-select" id="owner-field">
                                                         <option value="">Select industry type</option>
                                                         <option value="Computer Industry">Computer
                                                             Industry</option>
@@ -219,47 +217,51 @@
                                             </div>
                                             <div class="col-lg-4">
                                                 <div>
-                                                    <label for="star_value-field" class="form-label">Rating</label>
+                                                    <label for="star_value-field" class="form-label">Long</label>
                                                     <input type="text" id="star_value-field" class="form-control"
                                                         placeholder="Enter rating" required />
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div>
-                                                    <label for="location-field" class="form-label">location</label>
+                                                    <label for="location-field" class="form-label">Telefon</label>
                                                     <input type="text" id="location-field" class="form-control"
                                                         placeholder="Enter location" required />
                                                 </div>
                                             </div>
                                             <div class="col-lg-4">
                                                 <div>
-                                                    <label for="employee-field" class="form-label">Employee</label>
+                                                    <label for="employee-field" class="form-label">Açıklama</label>
                                                     <input type="text" id="employee-field" class="form-control"
                                                         placeholder="Enter employee" required />
                                                 </div>
                                             </div>
-                                            <div class="col-lg-6">
+                                             <div class="col-lg-6">
                                                 <div>
-                                                    <label for="website-field" class="form-label">Website</label>
-                                                    <input type="text" id="website-field" class="form-control"
-                                                        placeholder="Enter website" required />
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div>
-                                                    <label for="contact_email-field" class="form-label">Contact
-                                                        Email</label>
-                                                    <input type="text" id="contact_email-field" class="form-control"
-                                                        placeholder="Enter contact email" required />
+                                                    <label for="website-field" class="form-label">Firma</label>
+                                                    <select class="form-select" id="website-field">
+                                                        <option value="">Select industry type</option>
+                                                        <option value="Computer Industry">Computer
+                                                            Industry</option>
+                                                        <option value="Chemical Industries">Chemical
+                                                            Industries</option>
+                                                        <option value="Health Services">Health Services
+                                                        </option>
+                                                        <option value="Telecommunications Services">
+                                                            Telecommunications Services</option>
+                                                        <option value="Textiles: Clothing, Footwear">
+                                                            Textiles: Clothing, Footwear</option>
+                                                    </select>
                                                 </div>
                                             </div>
                                             <div class="col-lg-12">
                                                 <div>
-                                                    <label for="since-field" class="form-label">Since</label>
-                                                    <input type="text" id="since-field" class="form-control"
-                                                        placeholder="Enter since" required />
+                                                    <label for="contact_email-field" class="form-label">Adres</label>
+                                                    <input type="text" id="contact_email-field" class="form-control"
+                                                        placeholder="Enter contact email" required />
                                                 </div>
                                             </div>
+                                            
                                         </div>
                                     </div>
                                     <div class="modal-footer">
