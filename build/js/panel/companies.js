b0a4c59639045417c0ff160b4430aca2fe4b1192
MobilobyGit <info@mobiloby.com>
2024-03-12 17:35:42 +0300

    sdsadsad

--- build/js/panel/companies.js
+++ build/js/panel/companies.js
@@ -364,7 +364,7 @@ Array.prototype.slice.call(forms).forEach(function (form) {
                 formData.append('dealership_description', employeeField.value);
                 formData.append('dealership_address', contact_emailField.value);
                 formData.append('dealership_image_url', globalFile);
-                formData.append('dealership_iid',idField.value.replace("#VZ", ""));
+                formData.append('dealership_id',idField.value.replace("#VZ", ""));
                 $.ajax({
                     url: "https://mobiloby.app/koluman/web/updatedealership",
                     method: 'POST',
