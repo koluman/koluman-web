3206eba2c658cfe33394d2a852255976f0f73680
MobilobyGit <info@mobiloby.com>
2024-03-13 11:55:49 +0300

    dsfsdf

--- resources/views/ajans/announcementsdetail.blade.php
+++ resources/views/ajans/announcementsdetail.blade.php
@@ -38,6 +38,7 @@
                                     <option value="1">Haber</option>
                                     <option value="2">Duyuru</option>
                                     <option value="3">Kampanya</option>
+                                    <option value="-1">Hepsi</option>
                                 </select>
                             </div>
                             <div class="col-lg-4 mb-3">
