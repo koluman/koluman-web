86522932d767090110c767def14289084cdd5585
MobilobyGit <info@mobiloby.com>
2024-03-14 12:53:49 +0300

    sadas

--- build/js/panel/login.js
+++ build/js/panel/login.js
@@ -4,7 +4,7 @@ $( "#loginbutton" ).click(function() {
         var password = $("#password-input").val();
         $.ajax({
           type: 'POST',
-          url: 'koluman/web/login',
+          url: '/web/login',
           data: {
             email: email,
             password: password,
@@ -14,7 +14,7 @@ $( "#loginbutton" ).click(function() {
           success: function (data) {
             console.log(data);
             if (data.success == 1) {
-              window.location.href = "koluman/web/"+data.redirectRoute; 
+              window.location.href = "/web/"+data.redirectRoute; 
             } else {
                 alert(data.message); 
             }
