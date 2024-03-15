32300354685836711ecb56638a4de63321920551
MobilobyGit <info@mobiloby.com>
2024-03-15 14:09:08 +0300

    sdasdas

--- resources/views/admin/testdrive.blade.php
+++ resources/views/admin/testdrive.blade.php
@@ -212,8 +212,8 @@
 @endsection
 @section('script')
     <script src="{{ URL::asset('build/libs/sweetalert2/sweetalert2.min.js') }}"></script>
+        <script src="{{ URL::asset('build/js/app.js') }}"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="{{ URL::asset('build/libs/fullcalendar/index.global.min.js') }}"></script>
     <script src="{{ URL::asset('build/js/panel/testdrive.js') }}"></script>
-    <script src="{{ URL::asset('build/js/app.js') }}"></script>
 @endsection
